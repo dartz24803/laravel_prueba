7d8bb4fb614af1b13cab270511826287f5d4b53c
dartz24803 <pcardenasfuster@gmail.com>
2024-06-12 09:55:44 -0500

    cuadro de control visual

--- resources/views/tienda/administracion/CuadroControlVisual/Horarios/lista.blade.php
+++ resources/views/tienda/administracion/CuadroControlVisual/Horarios/lista.blade.php
@@ -14,14 +14,20 @@
             <tr class="text-center">
                 <td><?php echo $list['cod_base']; ?></td>
                 <td>
-                    <?php echo $list['puesto']; ?>
-                    <a href="javascript:void(0);" title="Agregar horario al puesto" data-toggle="modal" data-target="#ModalUpdate" app_elim="{{ url('Modal_Agregar_Horarios_Cuadro_Control/' . $list['id_horarios_cuadro_control']) }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle">
-                            <circle cx="12" cy="12" r="10"></circle>
-                            <line x1="12" y1="8" x2="12" y2="16"></line>
-                            <line x1="8" y1="12" x2="16" y2="12"></line>
-                        </svg>
-                    </a>
+                    <div class="d-flex flex-column justify-content-around">
+                        <div>
+                            <?php echo $list['puesto']; ?>
+                        </div>
+                        <div>
+                            <a href="javascript:void(0);" title="Agregar horario al puesto" data-toggle="modal" data-target="#ModalUpdate" app_elim="{{ url('Modal_Agregar_Horarios_Cuadro_Control/' . $list['id_horarios_cuadro_control']) }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle">
+                                    <circle cx="12" cy="12" r="10"></circle>
+                                    <line x1="12" y1="8" x2="12" y2="16"></line>
+                                    <line x1="8" y1="12" x2="16" y2="12"></line>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
                 </td>
                 <td>
                     <?php
