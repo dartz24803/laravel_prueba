0cca9d025a87bd59b90f96a47453355cccf1c168
Daniel <daniel11143118@gmail.com>
2024-06-13 17:26:59 -0500

    cambios

--- app/Models/ContenidoSeguimientoCoordinador.php
+++ app/Models/ContenidoSeguimientoCoordinador.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class ContenidoSeguimientoCoordinador extends Model
 {
     use HasFactory;
+
+    protected $table = 'contenido_seguimiento_coordinador';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'base',
+        'id_area',
+        'id_periocidad',
+        'nom_dia_1',
+        'nom_dia_2',
+        'nom_dia_3',
+        'dia_1',
+        'dia_2',
+        'dia',
+        'mes',
+        'descripcion',
+        'estado',
+        'fec_reg',
+        'user_reg',
+        'fec_act',
+        'user_act',
+        'fec_eli',
+        'user_eli'
+    ];
 }
