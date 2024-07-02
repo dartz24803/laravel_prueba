af85306b61cd124356dcd03cd6a5cfd20b227eb3
dartz24803 <pcardenasfuster@gmail.com>
2024-07-02 15:36:39 -0500

    inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -21,7 +21,7 @@
                                             <div class="zoom-card" style="width: 7rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #fea701;">
                                                     <div class="card-body text-center">
-                                                        <img src="" alt="">
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-Logistica.png')}}" alt="">
                                                     </div>
                                                     <p class="card-text text-center text-white">Logística</p>
                                                 </div>
@@ -32,7 +32,7 @@
                                             <div class="zoom-card mb-4" style="width: 8rem">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #ff295c; height: 8rem">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-Comercial.png')}}" alt="">
                                                     </div>
                                                     <p class="card-text text-center text-white">Comercial</p>
                                                 </div>
@@ -43,7 +43,7 @@
                                             <div class="zoom-card" style="width: 8rem; margin-top: -4rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #00b1f4; height:8rem;">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-package"><line x1="16.5" y1="9.4" x2="7.5" y2="4.21"></line><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-TalentoHumano.png')}}" alt="">
                                                     </div>
                                                     <p class="card-text text-center text-white">Talento Humano</p>
                                                 </div>
@@ -54,7 +54,7 @@
                                             <div class="zoom-card" style="width: 8rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #00ba8e; height: 7rem;">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck"><rect x="1" y="3" width="15" height="13"></rect><polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon><circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-Infraestructuras.png')}}" alt="">
                                                     </div>
                                                     <p class="card-text text-center text-white">Infraestructura</p>
                                                 </div>
@@ -65,7 +65,7 @@
                                             <div class="zoom-card" style="width: 7rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #00ba8e;">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck"><rect x="1" y="3" width="15" height="13"></rect><polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon><circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-Interna.png')}}" alt="">
                                                     </div>
                                                     <p class="card-text text-center text-white">Interna</p>
                                                 </div>
@@ -84,7 +84,7 @@
                                             <div class="zoom-card" style="width: 7rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #00b1f4;">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-Finanzas.png')}}" alt="">
                                                         <p class="card-text text-center text-white">Finanzas</p>
                                                     </div>
                                                 </div>
@@ -95,7 +95,7 @@
                                             <div class="zoom-card" style="width: 10rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #00b1f4; height:10rem">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-package"><line x1="16.5" y1="9.4" x2="7.5" y2="4.21"></line><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-SeguridadSalud.png')}}" alt="">
                                                         <p class="card-text text-center text-white">Seguridad y Salud</p>
                                                     </div>
                                                 </div>
@@ -106,7 +106,7 @@
                                             <div class="zoom-card" style="width: 7rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #fea701; height: 7rem; margin-top: 11rem">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-slack"><path d="M14.5 10c-.83 0-1.5-.67-1.5-1.5v-5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5v5c0 .83-.67 1.5-1.5 1.5z"></path><path d="M20.5 10H19V8.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"></path><path d="M9.5 14c.83 0 1.5.67 1.5 1.5v5c0 .83-.67 1.5-1.5 1.5S8 21.33 8 20.5v-5c0-.83.67-1.5 1.5-1.5z"></path><path d="M3.5 14H5v1.5c0 .83-.67 1.5-1.5 1.5S2 16.33 2 15.5 2.67 14 3.5 14z"></path><path d="M14 14.5c0-.83.67-1.5 1.5-1.5h5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5h-5c-.83 0-1.5-.67-1.5-1.5z"></path><path d="M15.5 19H14v1.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z"></path><path d="M10 9.5C10 8.67 9.33 8 8.5 8h-5C2.67 8 2 8.67 2 9.5S2.67 11 3.5 11h5c.83 0 1.5-.67 1.5-1.5z"></path><path d="M8.5 5H10V3.5C10 2.67 9.33 2 8.5 2S7 2.67 7 3.5 7.67 5 8.5 5z"></path></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-CajaControlInterno.png')}}" alt="">
                                                         <p class="card-text text-center text-white">Caja y Control Interno</p>
                                                     </div>
                                                 </div>
@@ -117,7 +117,7 @@
                                             <div class="zoom-card" style="width: 10rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #00ba8e; height: 10rem">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck"><rect x="1" y="3" width="15" height="13"></rect><polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon><circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-Manufactura.png')}}" alt="">
                                                     </div>
                                                     <p class="card-text text-center text-white">Manufactura</p>
                                                 </div>
@@ -128,7 +128,7 @@
                                             <div class="zoom-card" style="width: 8rem; margin-top: 2rem;">
                                                 <div class="card text-center border-0 rounded_z" style="background-color: #ff295c;height: 8rem">
                                                     <div class="card-body text-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#8ca1b5" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-slack"><path d="M14.5 10c-.83 0-1.5-.67-1.5-1.5v-5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5v5c0 .83-.67 1.5-1.5 1.5z"></path><path d="M20.5 10H19V8.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"></path><path d="M9.5 14c.83 0 1.5.67 1.5 1.5v5c0 .83-.67 1.5-1.5 1.5S8 21.33 8 20.5v-5c0-.83.67-1.5 1.5-1.5z"></path><path d="M3.5 14H5v1.5c0 .83-.67 1.5-1.5 1.5S2 16.33 2 15.5 2.67 14 3.5 14z"></path><path d="M14 14.5c0-.83.67-1.5 1.5-1.5h5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5h-5c-.83 0-1.5-.67-1.5-1.5z"></path><path d="M15.5 19H14v1.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z"></path><path d="M10 9.5C10 8.67 9.33 8 8.5 8h-5C2.67 8 2 8.67 2 9.5S2.67 11 3.5 11h5c.83 0 1.5-.67 1.5-1.5z"></path><path d="M8.5 5H10V3.5C10 2.67 9.33 2 8.5 2S7 2.67 7 3.5 7.67 5 8.5 5z"></path></svg>
+                                                        <img src="{{ asset('inicio/NEW.Intranet-Icono-Tiendas.png')}}" alt="">
                                                     </div>
                                                     <p class="card-text text-center text-white">Tiendas</p>
                                                 </div>
@@ -136,7 +136,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <!-- <img src="{{ asset('/inicio/Estamos-trabajando.png') }}" alt=""> -->
+                                <!-- <img src="{{ asset('/inicio/Estamos-trabajando.png') }}" alt="" style="max-width: 100%;"> -->
                             </div>
                         </div>
                     </div>
