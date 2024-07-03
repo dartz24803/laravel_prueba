13eb7ae6179d300ec0c274b7551b694e9c249602
Daniel <daniel11143118@gmail.com>
2024-07-03 15:04:07 -0500

    cambios

--- resources/views/seguridad/apertura_cierre/modal_registrar.blade.php
+++ resources/views/seguridad/apertura_cierre/modal_registrar.blade.php
@@ -38,7 +38,7 @@
                 <input type="time" class="form-control" value="{{ $get_hora->hora_ingreso }}" disabled>
             </div>
 
-            <div class="form-group col-lg-2">
+            <div class="form-group col-lg-2 d-flex justify-content-center d-lg-block">
                 <button type="button" class="btn btn-secondary" id="boton_camara" onclick="Activar_Camara();">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-camera"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
                 </button>
