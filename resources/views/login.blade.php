5e48dac02f0d78e5440fc0a984f0ecbc346cb4ac
dartz24803 <pcardenasfuster@gmail.com>
2024-07-08 11:50:34 -0500

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,6 +23,27 @@
 
   <body>
     <div class="main">
+    <div style="padding-top: 1rem; margin-left:1rem">
+        <div style="display: flex; justify-content: space-between;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#ffa700" id="record">
+                <path d="M12 17a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z"></path>
+                <path fill="#ffa700" fill-rule="evenodd" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-2 0a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" clip-rule="evenodd"></path>
+            </svg>
+            <svg id="SvgjsSvg1001" width="32" height="32" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs">
+                <defs id="SvgjsDefs1002"></defs>
+                <g id="SvgjsG1008">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="333" height="333">
+                        <path fill="#ffa700" d="M12 17a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z" class="color000 svgShape"></path>
+                        <path fill="#ffa700" fill-rule="evenodd" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-2 0a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" clip-rule="evenodd" class="color000 svgShape"></path>
+                    </svg>
+                </g>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#ffa700" id="record">
+                <path d="M12 17a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z"></path>
+                <path fill="#ffa700" fill-rule="evenodd" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-2 0a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" clip-rule="evenodd"></path>
+            </svg>
+        </div>
+    </div>
     <img class="pequena" src="{{ asset('login_files/img/grupo.png')}}" alt="IMG">
         <div class="padre2">
           <label class="hijo2 bienvenido">
@@ -32,24 +53,20 @@
         </div>
 
         <form class="form1" id="frm_login" name="frm_login" action="{{ route('IngresarLogin')}}" method="post">
-            <div class="col-md-12" style="display: flex; justify-content: center;">
-                <strong><label for="Usuario" style="color: #888ea8;">USUARIO</label></strong>
+            <div class="col-md-12" style="display: flex; justify-content: center;margin-top: 0.7rem;">
+                <strong><label for="Usuario" style="color: #696769; font-size:smaller">USUARIO</label></strong>
             </div>
             <input type="text" class="un" id="Usuario" name="Usuario" align="center">
             <div class="col-md-12" style="display: flex; justify-content: center;">
-                <strong><label for="Password" style="color: #888ea8;">CONTRASEÑA</label></strong>
+                <strong><label for="Password" style="color: #696769; font-size:smaller">CONTRASEÑA</label></strong>
             </div>
             <input type="password" class="pass" id="Password"  name="Password"  align="center">
 
 
-            <center style="background-color: white;"><br><div class="forgot-password"><a style="color:#F07D00" >¿Olvidaste&nbsp;tu&nbsp;contraseña?</a></div></center><br>
+            <center style="background-color: white;"><br><div class="forgot-password"><a style="color:#ffa404; font-weight:600; font-size:0.7rem;" >¿Olvidaste&nbsp;tu&nbsp;contraseña?</a></div></center><br>
             <center style="background-color: white;"><span class="txt1" role="alert" id="resultado" style="color:red;"></span></center>
             <button type="submit" class="submit" value="Login" name="login" id="submit" align="center">INGRESAR</button>
         </form>
-        <!-- <div style="display: flex; justify-content: space-between; margin-top: 1rem;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#ffa700" stroke="#ffa700" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#ffa700" stroke="#ffa700" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
-        </div> -->
       </div>
   </body>
 </html>
