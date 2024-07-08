18a0b7a40f92c8c2e157b2f08d5f54669206cc83
Daniel Palomino <daniel11143118@gmail.com>
2024-07-08 14:56:10 -0500

    cambios

--- config/database.php
+++ config/database.php
@@ -115,9 +115,6 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'options' => [
-                'TrustServerCertificate' => true,
-            ]
         ],
 
     ],
