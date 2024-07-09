c9a046e207e8b2942ffcaa0d9e7e7bf00b7000b1
dartz24803 <pcardenasfuster@gmail.com>
2024-07-09 15:16:45 -0500

    inicio

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -47,7 +47,7 @@
         <header class="header navbar navbar-expand-sm expand-header d-flex justify-content-around" style="background: #302f30; height:5rem;">
             <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>
 
-            <a class="col-md-5 offset-5">
+            <a id="div_imagen_header" class="col-md-5 offset-5">
                 <img src="{{ asset('inicio/Grupo-LN1.png') }}" class="navbar-logo ajuste1" alt="logo">
             </a>
             <ul class="navbar-item flex-row ml-auto">
@@ -583,6 +583,11 @@
     <style>
     .dkuywW{
         display:none !important;
+    }
+    @media (max-width: 600px) {
+        #div_imagen_header{
+            display:none
+        }
     }
     </style>
     <script src="{{ asset('template/plugins/select2/select2.min.js') }}"></script>
