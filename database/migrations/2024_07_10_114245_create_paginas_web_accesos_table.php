e9aa0717359c812240afd7162ec137d1a54caf4c
dartz24803 <pcardenasfuster@gmail.com>
2024-07-10 11:45:24 -0500

    accesos

---
+++ database/migrations/2024_07_10_114245_create_paginas_web_accesos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('paginas_web_accesos', function (Blueprint $table) {
+            $table->id(); // Crea una columna 'id' que es auto incremental y clave primaria
+            $table->integer('area')->nullable();
+            $table->integer('puesto')->nullable();
+            $table->string('pagina_acceso', 255)->nullable();
+            $table->integer('estado')->nullable();
+            $table->integer('user_reg')->nullable();
+            $table->dateTime('fec_reg')->nullable();
+            $table->integer('user_act')->nullable();
+            $table->dateTime('fec_act')->nullable();
+            $table->integer('user_eli')->nullable();
+            $table->dateTime('fec_eli')->nullable();
+            $table->foreign('puesto')->references('id_puesto')->on('puesto');
+            $table->foreign('area')->references('id_area')->on('area');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('paginas_web_accesos');
+    }
+};
---
+++ database/migrations/2024_07_10_114405_create_programas_accesos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programas_accesos', function (Blueprint $table) {
+            $table->id(); // Crea una columna 'id' que es auto incremental y clave primaria
+            $table->integer('area')->nullable();
+            $table->integer('puesto')->nullable();
+            $table->string('programa', 255)->nullable();
+            $table->integer('estado')->nullable();
+            $table->dateTime('fec_reg')->nullable();
+            $table->integer('user_reg')->nullable();
+            $table->dateTime('fec_act')->nullable();
+            $table->integer('user_act')->nullable();
+            $table->dateTime('fec_eli')->nullable();
+            $table->integer('user_eli')->nullable();
+            $table->foreign('puesto')->references('id_puesto')->on('puesto');
+            $table->foreign('area')->references('id_area')->on('area');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programas_accesos');
+    }
+};
