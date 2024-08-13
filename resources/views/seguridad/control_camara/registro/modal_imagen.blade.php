2c87566d188aa9c9e4da53a9805696964186f4a7
dartz24803 <pcardenasfuster@gmail.com>
2024-08-13 11:20:52 -0500

    control camaras

--- resources/views/seguridad/control_camara/registro/modal_imagen.blade.php
+++ resources/views/seguridad/control_camara/registro/modal_imagen.blade.php
@@ -14,7 +14,7 @@
 
 <form id="formularioi" method="POST" enctype="multipart/form-data" class="needs-validation">
     <div class="modal-header">
-        <h5 class="modal-title">Nuevo control de cámaras</h5>
+        <h5 class="modal-title">Control de cámaras Bases</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
         </button>
