ca86c9643c6efdf06029bab71bf4da0bade78315
Daniel <daniel11143118@gmail.com>
2024-08-14 15:51:43 -0500

    cambios

---
+++ resources/views/control_interno/administracion/precio_sugerido/precio_tres/modal_editar.blade.php
@@ -0,0 +1,107 @@
+<form id="formularioe" method="POST" enctype="multipart/form-data" class="needs-validation">
+    <div class="modal-header">
+        <h5 class="modal-title">Editar precio x 2:</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+        </button>
+    </div> 
+
+    <div class="modal-body" style="max-height:700px; overflow:auto;">
+        <div class="row">
+            <div class="form-group col-lg-2">
+                <label>Base:</label>
+            </div>
+            <div class="form-group col-lg-4">
+                <select class="form-control" name="id_basee" id="id_basee">
+                    <option value="0">Seleccione</option>
+                    @foreach ($list_base as $list)
+                        <option value="{{ $list->id_base }}" 
+                        @if ($list->id_base==$get_id->id_base) selected @endif>
+                            {{ $list->cod_base }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="form-group col-lg-2">
+                <label>Precio x 1:</label>
+            </div>
+            <div class="form-group col-lg-4">
+                <input type="text" class="form-control" id="precio_1e" name="precio_1e" placeholder="Precio x 1" 
+                value="{{ $get_id->precio_1 }}" onkeypress="return solo_Numeros_Punto(event);">
+            </div>
+        </div> 	 	
+        
+        <div class="row">
+            <div class="form-group col-lg-2">
+                <label>Precio x 2:</label>
+            </div>
+            <div class="form-group col-lg-4">
+                <input type="text" class="form-control" id="precio_2e" name="precio_2e" placeholder="Precio x 2" 
+                value="{{ $get_id->precio_2 }}" onkeypress="return solo_Numeros_Punto(event);">
+            </div>
+
+            <div class="form-group col-lg-2">
+                <label>Precio x 3:</label>
+            </div>
+            <div class="form-group col-lg-4">
+                <input type="text" class="form-control" id="precio_3e" name="precio_3e" placeholder="Precio x 3" 
+                value="{{ $get_id->precio_3 }}" onkeypress="return solo_Numeros_Punto(event);">
+            </div>
+        </div>
+    </div>
+
+    <div class="modal-footer">
+        @csrf
+        @method('PUT')
+        <button class="btn btn-primary" type="button" onclick="Update_Precio_Tres();">Guardar</button>
+        <button class="btn" data-dismiss="modal"><i class="flaticon-cancel-12"></i> Cancelar</button>
+    </div>
+</form>
+
+<script>
+    function Update_Precio_Tres() {
+        Cargando();
+
+        var dataString = new FormData(document.getElementById('formularioe'));
+        var url = "{{ route('precio_sugerido_conf_tr.update', $get_id->id) }}";
+
+        $.ajax({
+            url: url,
+            data: dataString,
+            type: "POST",
+            processData: false,
+            contentType: false,
+            success: function(data) {
+                if(data=="error"){
+                    Swal({
+                        title: '¡Actualización Denegada!',
+                        text: "¡El registro ya existe!",
+                        type: 'error',
+                        showCancelButton: false,
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'OK',
+                    });
+                }else{
+                    swal.fire(
+                        '¡Actualización Exitosa!',
+                        '¡Haga clic en el botón!',
+                        'success'
+                    ).then(function() {
+                        Lista_Precio_Tres();
+                        $("#ModalUpdate .close").click();
+                    });  
+                }
+            },
+            error:function(xhr) {
+                var errors = xhr.responseJSON.errors;
+                var firstError = Object.values(errors)[0][0];
+                Swal.fire(
+                    '¡Ups!',
+                    firstError,
+                    'warning'
+                );
+            }
+        });
+    }
+</script>
