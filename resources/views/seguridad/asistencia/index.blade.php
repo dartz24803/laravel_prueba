d61415fa649ee53955cf3ff1258ad1b5cb43e303
Daniel <daniel11143118@gmail.com>
2024-08-20 09:54:24 -0500

    cambios

--- resources/views/seguridad/asistencia/index.blade.php
+++ resources/views/seguridad/asistencia/index.blade.php
@@ -23,7 +23,7 @@
                                 session('usuario')->id_puesto==197 || 
                                 session('usuario')->id_puesto==148)
                                     <li class="nav-item">
-                                        <a id="a_lec" class="nav-link" onclick="Lectura();" style="cursor: pointer;">Lectora (Asistencia con lectora)</a>
+                                        <a id="a_lec" class="nav-link" onclick="Lectora();" style="cursor: pointer;">Lectora (Asistencia con lectora)</a>
                                     </li>
                                 @endif
                                 @if (session('usuario')->id_puesto===10 || 
@@ -36,17 +36,17 @@
                                 session('usuario')->id_puesto===155|| 
                                 session('usuario')->id_puesto===134)
                                     <li class="nav-item">
-                                        <a id="a_ges" class="nav-link" onclick="Gestion();" style="cursor: pointer;">Manual</a>
+                                        <a id="a_man" class="nav-link" onclick="Manual();" style="cursor: pointer;">Manual</a>
                                     </li>
                                 @endif
                                 <li class="nav-item">
-                                    <a id="a_ges" class="nav-link" onclick="Gestion();" style="cursor: pointer;">Reporte</a>
+                                    <a id="a_rep" class="nav-link" onclick="Reporte();" style="cursor: pointer;">Reporte</a>
                                 </li>
                             </ul>
 
                             <div class="row" id="cancel-row">
                                 <div class="col-xl-12 col-lg-12 col-sm-12 layout-spacing">
-                                    <div id="div_lectura_servicio" class="widget-content widget-content-area p-3">
+                                    <div id="div_asistencia" class="widget-content widget-content-area p-3">
                                     </div>
                                 </div>
                             </div>
@@ -63,20 +63,10 @@
             $("#hseguridades").attr('aria-expanded', 'true');
             $("#asistencias_segs").addClass('active');
 
-            @if(session('usuario')->id_puesto==24 || session('usuario')->id_puesto==36 || 
-            session('usuario')->id_puesto==29 || session('usuario')->id_puesto==16 ||
-            session('usuario')->id_puesto==20 || session('usuario')->id_puesto==26 || 
-            session('usuario')->id_puesto==27 || session('usuario')->id_puesto==98 ||
-            session('usuario')->id_puesto==31 || session('usuario')->id_puesto==30 || 
-            session('usuario')->id_nivel==1 || session('usuario')->id_puesto==161 || 
-            session('usuario')->id_puesto==197 || session('usuario')->id_puesto==148)
-                Lectura();
-            @else
-                Gestion()
-            @endif
+            Lectora();
         });
         
-        function Lectura(){
+        function Lectora(){
             Cargando();
 
             var url="{{ route('lectura_servicio_reg') }}";
@@ -85,14 +75,15 @@
                 url: url,
                 type: "GET",
                 success:function (resp) {
-                    $('#div_lectura_servicio').html(resp);  
+                    $('#div_asistencia').html(resp);  
                     $("#a_lec").addClass('active');
-                    $("#a_ges").removeClass('active');
+                    $("#a_man").removeClass('active');
+                    $("#a_rep").removeClass('active');
                 }
             });
         }
 
-        function Gestion(){
+        function Manual(){
             Cargando();
 
             var url="{{ route('lectura_servicio_ges') }}";
@@ -101,9 +92,27 @@
                 url: url,
                 type: "GET",
                 success:function (resp) {
-                    $('#div_lectura_servicio').html(resp);  
+                    $('#div_asistencia').html(resp);  
                     $("#a_lec").removeClass('active');
-                    $("#a_ges").addClass('active');
+                    $("#a_man").addClass('active');
+                    $("#a_rep").removeClass('active');
+                }
+            });
+        }
+
+        function Reporte(){
+            Cargando();
+
+            var url="{{ route('lectura_servicio_ges') }}";
+
+            $.ajax({
+                url: url,
+                type: "GET",
+                success:function (resp) {
+                    $('#div_asistencia').html(resp);  
+                    $("#a_lec").removeClass('active');
+                    $("#a_man").removeClass('active');
+                    $("#a_rep").addClass('active');
                 }
             });
         }
