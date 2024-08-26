a0141785ad087aa51ea24084190519f9f8292690
dartz24803 <pcardenasfuster@gmail.com>
2024-08-26 10:00:53 -0500

    logistica

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -451,12 +451,12 @@
                                         </a>
                                     </li>
                                 <?php } ?>
-                                <?php if (session('usuario')->id_nivel == 1 || 
-                                session('usuario')->id_nivel == 2 || 
-                                session('usuario')->id_puesto == 21 || 
-                                session('usuario')->id_puesto == 278 || 
-                                session('usuario')->id_puesto == 279 || 
-                                session('usuario')->id_puesto == 22 || 
+                                <?php if (session('usuario')->id_nivel == 1 ||
+                                session('usuario')->id_nivel == 2 ||
+                                session('usuario')->id_puesto == 21 ||
+                                session('usuario')->id_puesto == 278 ||
+                                session('usuario')->id_puesto == 279 ||
+                                session('usuario')->id_puesto == 22 ||
                                 session('usuario')->id_puesto == 133) { ?>
                                     <!--<li>
                                         <a id="postulantes_revisiones" href="{{ route('postulante_revision') }}">
@@ -503,10 +503,10 @@
                                         <p class="romperpalabra"><span id="icono_active2"></span> Lectura Servicio</p>
                                     </a>
                                 </li>
-                                
+
                             <?php if (
                                 session('usuario')->id_puesto == 23 || session('usuario')->id_puesto == 24 || session('usuario')->id_puesto == 36 ||
-                                session('usuario')->id_puesto == 26 || session('usuario')->id_puesto == 29 || session('usuario')->id_puesto == 161 || 
+                                session('usuario')->id_puesto == 26 || session('usuario')->id_puesto == 29 || session('usuario')->id_puesto == 161 ||
                                 session('usuario')->id_nivel == 1 || session('usuario')->id_puesto == 197 || session('usuario')->id_puesto == 148                            ) { ?>
                                 <li id="locurrencia">
                                     <a id="hlocurrencia" href="<?= url('OcurrenciaTienda/index') ?>">
@@ -514,8 +514,8 @@
                                     </a>
                                 </li>
                             <?php } ?>
-                            
-                            <?php if (session('usuario')->id_puesto == 23 || session('usuario')->id_puesto == 24 || 
+
+                            <?php if (session('usuario')->id_puesto == 23 || session('usuario')->id_puesto == 24 ||
                             session('usuario')->id_nivel == 1 || session('usuario')->id_puesto == 36) { ?>
                                 <li id="lrproveedor">
                                     <a id="hlrproveedor" href="<?= url('RProveedores/index') ?>">
@@ -523,7 +523,7 @@
                                     </a>
                                 </li>
                             <?php } ?>
-                               
+
                             </ul>
                         </li>
 
@@ -577,6 +577,36 @@
                                 </ul>
                             </li>
                         <?php } ?>
+
+                    <?php if (session('usuario')->id_nivel == 1 || session('usuario')->id_nivel == 4 || session('usuario')->id_puesto == 68) { ?>
+                        <li class="menu" id="marketing">
+                            <a href="#rmarketing" id="hmarketing" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                                <div class="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart">
+                                        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+                                    </svg>
+                                    <span>Marketing</span>
+                                </div>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right">
+                                        <polyline points="9 18 15 12 9 6"></polyline>
+                                    </svg>
+                                </div>
+                            </a>
+                            <ul class="collapse submenu list-unstyled" id="rmarketing" data-parent="#accordionExample">
+                                <li id="sliderc">
+                                    <a id="hslider" href="{{ url('Corporacion/Slider_List_Comercial') }}">
+                                        <p class="romperpalabra"><span id="icono_active2"></span> Slider Marketing</p>
+                                    </a>
+                                </li>
+                                <li id="mercaderiamkt">
+                                    <a id="rmercaderiamkt" href="{{ url('Logistica/Mercaderia_Fotografia/2') }}">
+                                        <p class="romperpalabra" title="Mercadería a enviar para fotografía"><span id="icono_active2"></span> Mercadería a Enviar para fotografía</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    <?php } ?>
                         <!-- Administrables  -->
                         <li class="menu menu-heading">
                             <div class="heading">
@@ -736,7 +766,7 @@
                                     <p class="romperpalabra"><span id="icono_active2"></span> Ocurrencias</p>
                                 </a>
                             </li>
-                            
+
                         </ul>
                     </li>
 
