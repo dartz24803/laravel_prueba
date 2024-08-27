bac0531b4fee96649963b8cb306f76f4d07acba2
Daniel <daniel11143118@gmail.com>
2024-08-27 12:29:27 -0500

    cambios

--- resources/views/seguridad/inicio_seguridad.blade.php
+++ resources/views/seguridad/inicio_seguridad.blade.php
@@ -16,8 +16,8 @@
 </div>
 <script>
     $(document).ready(function() {
-        $("#inicio").addClass('active');
-        $("#hinicio").attr('aria-expanded', 'true');
+        $("#seguridades").addClass('active');
+        $("#hseguridades").attr('aria-expanded', 'true');
     });
 </script>
 @endsection
