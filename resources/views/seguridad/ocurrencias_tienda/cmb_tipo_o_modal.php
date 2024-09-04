139dc812e8f95d04dc81ac2abf2840ad6fc216a4
dartz24803 <pcardenasfuster@gmail.com>
2024-09-04 17:19:58 -0500

    ocurrencias

--- resources/views/seguridad/ocurrencias_tienda/cmb_tipo_o_modal.php
+++ resources/views/seguridad/ocurrencias_tienda/cmb_tipo_o_modal.php
@@ -1,7 +1,7 @@
 
 <label>Tipo Ocurrencia</label>
 <select class="form-control basic" id="tipo_ocurrencia_busq" name="tipo_ocurrencia_busq">                                       
-    <option value="0">Seleccione</option>
+    <option value="Todo">Todos</option>
     <?php foreach($list_tipo as $list){ ?>
         <option value="<?php echo $list->id_tipo_ocurrencia; ?>"><?php echo $list->nom_tipo_ocurrencia; ?></option>  
     <?php } ?>
