0126297eeb06d1a326d31e8f3d0155436f8ab1b4
Daniel <daniel11143118@gmail.com>
2024-09-04 16:44:33 -0500

    cambios

--- app/Models/Entrenamiento.php
+++ app/Models/Entrenamiento.php
@@ -64,13 +64,13 @@ class Entrenamiento extends Model
                     CASE WHEN (SELECT COUNT(1) FROM examen_entrenamiento ee 
                     WHERE ee.id_entrenamiento=en.id AND ee.fecha_revision IS NOT NULL AND ee.estado=1)>0 THEN
                         (CASE WHEN (SELECT ee.nota FROM examen_entrenamiento ee 
-                        WHERE ee.id_entrenamiento=en.id AND ee.estado=1)>=15 THEN 'Aprobado' 
+                        WHERE ee.id_entrenamiento=en.id AND ee.estado=1)>=14 THEN 'Aprobado' 
                         ELSE 'Rechazado' END)
                     ELSE '' END AS nom_evaluacion,
                     (SELECT COUNT(1) FROM examen_entrenamiento ee 
                     WHERE ee.id_entrenamiento=en.id AND ee.estado=1) AS examen_asignado,
                     CASE WHEN (SELECT COUNT(1) FROM pregunta pr 
-                    WHERE pr.id_puesto=sp.id_puesto_aspirado AND pr.id_tipo=1 AND pr.estado=1)>=15 AND 
+                    WHERE pr.id_puesto=sp.id_puesto_aspirado AND pr.id_tipo=1 AND pr.estado=1)>=14 AND 
                     (SELECT COUNT(1) FROM pregunta pr 
                     WHERE pr.id_puesto=sp.id_puesto_aspirado AND pr.id_tipo=2 AND pr.estado=1)>=5 THEN 1
                     ELSE 0 END AS examen_acceso,sp.id_puesto_aspirado
