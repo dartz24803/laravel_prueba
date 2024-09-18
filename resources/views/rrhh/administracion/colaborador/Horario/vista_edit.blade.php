dd28a4f0b06c5dd44c996bda3be0b90b4c95ade9
dartz24803 <pcardenasfuster@gmail.com>
2024-09-18 17:40:12 -0500

    lectura servicio

--- resources/views/rrhh/administracion/colaborador/Horario/vista_edit.blade.php
+++ resources/views/rrhh/administracion/colaborador/Horario/vista_edit.blade.php
@@ -1,6 +1,6 @@
 <form id="formulario_horario_u" method="POST" enctype="multipart/form-data" class="needs-validation">
     <div class="modal-header">
-        <h5 class="modal-title">Registrar Nuevo Horario</h5>
+        <h5 class="modal-title">Editar Horario</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
         </button>
@@ -176,7 +176,7 @@
                 <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home_u" role="tab" aria-controls="home" aria-selected="true">Conf. Básica</a>
             </li>
         </ul>
-
+        {{-- Horarios detalle --}}
         <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="home_u" role="tabpanel" aria-labelledby="home-tab">
                 <div class="col-md-12 row ">
@@ -185,113 +185,62 @@
                         <label class="control-label text-bold">Lunes: </label>
                     </div>
                     <div class="form-group col-md-4 conf_basica_lu_u">
-                        <?php 
-                        $busq_detalle = in_array('1', array_column($get_detalle, 'dia'));
-                        $posicion = array_search('1', array_column($get_detalle, 'dia'));
-                        ?>
                         <select name="id_turno_lu_u" id="id_turno_lu_u" class="form-control">
                             <option value="0">Seleccione</option>
-                            <?php foreach($list_turno as $list){?>
-                            <option value="<?php echo $list['id_turno'] ?>" <?php if($busq_detalle!=false){if($get_detalle[$posicion]['id_turno']==$list['id_turno']){echo "selected";}}?>><?php echo $list['option_select'] ?></option>
-                            <?php }?>
                         </select>
                     </div>
                 
-                    <?php 
-                        $busq_detalle = in_array('2', array_column($get_detalle, 'dia'));
-                        $posicion = array_search('2', array_column($get_detalle, 'dia'));
-                    ?>
                     <div class="form-group col-md-2 conf_basica_ma_u">
                         <label class="control-label text-bold">Martes: </label>
                     </div>
                     <div class="form-group col-md-4 conf_basica_ma_u">
                         <select name="id_turno_ma_u" id="id_turno_ma_u" class="form-control">
                             <option value="0">Seleccione</option>
-                            <?php foreach($list_turno as $list){?>
-                            <option value="<?php echo $list['id_turno'] ?>" <?php if($busq_detalle!=false){if($get_detalle[$posicion]['id_turno']==$list['id_turno']){echo "selected";}}?>><?php echo $list['option_select'] ?></option>
-                            <?php }?>
                         </select>
                     </div>
                 
-                    <?php 
-                        $busq_detalle = in_array('3', array_column($get_detalle, 'dia'));
-                        $posicion = array_search('3', array_column($get_detalle, 'dia'));
-                        
-                    ?>
                     <div class="form-group col-md-2 conf_basica_mi_u">
                         <label class="control-label text-bold">Miércoles: </label>
                     </div>
                     <div class="form-group col-md-4 conf_basica_mi_u">
                         <select name="id_turno_mi_u" id="id_turno_mi_u" class="form-control">
                             <option value="0">Seleccione</option>
-                            <?php foreach($list_turno as $list){?>
-                            <option value="<?php echo $list['id_turno'] ?>" <?php if($busq_detalle!=false){if($get_detalle[$posicion]['id_turno']==$list['id_turno']){echo "selected";}}?>><?php echo $list['option_select'] ?></option>
-                            <?php }?>
                         </select>
                     </div>
                 
-                    <?php 
-                        $busq_detalle = in_array('4', array_column($get_detalle, 'dia'));
-                        $posicion = array_search('4', array_column($get_detalle, 'dia'));
-                    ?>
                     <div class="form-group col-md-2 conf_basica_ju_u">
                         <label class="control-label text-bold">Jueves: </label>
                     </div>
                     <div class="form-group col-md-4 conf_basica_ju_u">
                         <select name="id_turno_ju_u" id="id_turno_ju_u" class="form-control">
                             <option value="0">Seleccione</option>
-                            <?php foreach($list_turno as $list){?>
-                            <option value="<?php echo $list['id_turno'] ?>" <?php if($busq_detalle!=false){if($get_detalle[$posicion]['id_turno']==$list['id_turno']){echo "selected";}}?>><?php echo $list['option_select'] ?></option>
-                            <?php }?>
                         </select>
                     </div>
 
-                    <?php 
-                        $busq_detalle = in_array('5', array_column($get_detalle, 'dia'));
-                        $posicion = array_search('5', array_column($get_detalle, 'dia'));
-                    ?>
                     <div class="form-group col-md-2 conf_basica_vi_u">
                         <label class="control-label text-bold">Viernes: </label>
                     </div>
                     <div class="form-group col-md-4 conf_basica_vi_u">
                         <select name="id_turno_vi_u" id="id_turno_vi_u" class="form-control">
                             <option value="0">Seleccione</option>
-                            <?php foreach($list_turno as $list){?>
-                            <option value="<?php echo $list['id_turno'] ?>" <?php if($busq_detalle!=false){if($get_detalle[$posicion]['id_turno']==$list['id_turno']){echo "selected";}}?>><?php echo $list['option_select'] ?></option>
-                            <?php }?>
                         </select>
                     </div>
                     
-                    <?php 
-                        $busq_detalle = in_array('6', array_column($get_detalle, 'dia'));
-                        $posicion = array_search('6', array_column($get_detalle, 'dia'));
-                        
-                    ?>
                     <div class="form-group col-md-2 conf_basica_sa_u">
                         <label class="control-label text-bold">Sábado: </label>
                     </div>
                     <div class="form-group col-md-4 conf_basica_sa_u">
                         <select name="id_turno_sa_u" id="id_turno_sa_u" class="form-control">
                             <option value="0">Seleccione</option>
-                            <?php foreach($list_turno as $list){?>
-                            <option value="<?php echo $list['id_turno'] ?>" <?php if($busq_detalle!=false){if($get_detalle[$posicion]['id_turno']==$list['id_turno']){echo "selected";}}?>><?php echo $list['option_select'] ?></option>
-                            <?php }?>
                         </select>
                     </div>
                     
-                    <?php 
-                        $busq_detalle = in_array('7', array_column($get_detalle, 'dia'));
-                        $posicion = array_search('7', array_column($get_detalle, 'dia'));
-                    ?>
-                    <div class="form-group col-md-2 conf_basica_do_u">
+                        <div class="form-group col-md-2 conf_basica_do_u">
                         <label class="control-label text-bold">Domingo: </label>
                     </div>
                     <div class="form-group col-md-4 conf_basica_do_u">
                         <select name="id_turno_do_u" id="id_turno_do_u" class="form-control">
                             <option value="0">Seleccione</option>
-                            <?php foreach($list_turno as $list){?>
-                            <option value="<?php echo $list['id_turno'] ?>" <?php if($busq_detalle!=false){if($get_detalle[$posicion]['id_turno']==$list['id_turno']){echo "selected";}}?>><?php echo $list['option_select'] ?></option>
-                            <?php }?>
                         </select>
                     </div>
                     
@@ -309,6 +258,7 @@
 
 <script>
     $(document).ready(function() {
+        Busca_Turno_XBase('_u');
         <?php 
             $busq_detalle = in_array('1', array_column($get_detalle, 'dia'));
             $posicion = array_search('1', array_column($get_detalle, 'dia'));
@@ -382,49 +332,10 @@
     }
 
     function Update_Horario() {
-        $(document)
-        .ajaxStart(function() {
-            $.blockUI({
-                message: '<svg> ... </svg>',
-                fadeIn: 800,
-                overlayCSS: {
-                    backgroundColor: '#1b2024',
-                    opacity: 0.8,
-                    zIndex: 1200,
-                    cursor: 'wait'
-                },
-                css: {
-                    border: 0,
-                    color: '#fff',
-                    zIndex: 1201,
-                    padding: 0, 
-                    backgroundColor: 'transparent'
-                }
-            });
-        })
-        .ajaxStop(function() {
-            $.blockUI({
-                message: '<svg> ... </svg>',
-                fadeIn: 800,
-                timeout: 100,
-                overlayCSS: {
-                    backgroundColor: '#1b2024',
-                    opacity: 0.8,
-                    zIndex: 1200,
-                    cursor: 'wait'
-                },
-                css: {
-                    border: 0,
-                    color: '#fff',
-                    zIndex: 1201,
-                    padding: 0,
-                    backgroundColor: 'transparent'
-                }
-            });
-        });
-
+        Cargando();
         var dataString = new FormData(document.getElementById('formulario_horario_u'));
         var url = "{{ url('ColaboradorConfController/Update_Horario') }}";
+        var csrfToken = $('input[name="_token"]').val();
 
         if (Valida_Update_Horario()) {
             $.ajax({
@@ -433,6 +344,9 @@
                 type: "POST",
                 processData: false,
                 contentType: false,
+                headers: {
+                    'X-CSRF-TOKEN': csrfToken
+                },
                 success: function(data) {
                     swal.fire(
                         'Actualización Exitoso!',
@@ -526,4 +440,70 @@
         }
         return true;
     }
+    
+    function Busca_Turno_XBase(v) {
+       Cargando();
+        var cod_base=$('#cod_base'+v).val();
+        var url = "{{ url('ColaboradorConfController/Busca_Turno_XBase') }}";
+        var csrfToken = $('input[name="_token"]').val();
+        $.ajax({
+            type: "POST",
+            data:{'cod_base':cod_base},
+            url: url,
+            headers: {
+                'X-CSRF-TOKEN': csrfToken
+            },
+            success: function(data) {
+                $('#id_turno_lu'+v).html(data);
+                <?php 
+                    $busq_detalle = in_array('1', array_column($get_detalle, 'dia'));
+                    $posicion = array_search('1', array_column($get_detalle, 'dia'));
+                ?>
+                $('#id_turno_lu'+v).val(<?= $get_detalle[$posicion]['id_turno'] ?>);
+
+                $('#id_turno_ma'+v).html(data);
+                <?php 
+                    $busq_detalle = in_array('2', array_column($get_detalle, 'dia'));
+                    $posicion = array_search('2', array_column($get_detalle, 'dia'));
+                ?>
+                $('#id_turno_ma'+v).val(<?= $get_detalle[$posicion]['id_turno'] ?>);
+
+                $('#id_turno_mi'+v).html(data);
+                <?php 
+                    $busq_detalle = in_array('3', array_column($get_detalle, 'dia'));
+                    $posicion = array_search('3', array_column($get_detalle, 'dia'));
+                ?>
+                $('#id_turno_mi'+v).val(<?= $get_detalle[$posicion]['id_turno'] ?>);
+
+                $('#id_turno_ju'+v).html(data);
+                <?php 
+                    $busq_detalle = in_array('4', array_column($get_detalle, 'dia'));
+                    $posicion = array_search('4', array_column($get_detalle, 'dia'));
+                ?>
+                $('#id_turno_ju'+v).val(<?= $get_detalle[$posicion]['id_turno'] ?>);
+
+                $('#id_turno_vi'+v).html(data);
+                <?php 
+                    $busq_detalle = in_array('5', array_column($get_detalle, 'dia'));
+                    $posicion = array_search('5', array_column($get_detalle, 'dia'));
+                ?>
+                $('#id_turno_vi'+v).val(<?= $get_detalle[$posicion]['id_turno'] ?>);
+
+                $('#id_turno_sa'+v).html(data);
+                <?php 
+                    $busq_detalle = in_array('6', array_column($get_detalle, 'dia'));
+                    $posicion = array_search('6', array_column($get_detalle, 'dia'));
+                ?>
+                $('#id_turno_sa'+v).val(<?= $get_detalle[$posicion]['id_turno'] ?>);
+
+                $('#id_turno_do'+v).html(data);
+                <?php 
+                    $busq_detalle = in_array('7', array_column($get_detalle, 'dia'));
+                    $posicion = array_search('7', array_column($get_detalle, 'dia'));
+                ?>
+                $('#id_turno_do'+v).val(<?= $get_detalle[$posicion]['id_turno'] ?>);
+
+            }
+        });
+    } 
 </script>
