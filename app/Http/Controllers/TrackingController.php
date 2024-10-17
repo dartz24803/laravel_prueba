6df98ab9bf2a3b47911566cd27dbab4b35a4ba87
Daniel <daniel11143118@gmail.com>
2024-10-17 09:15:58 -0500

    cambios

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -161,7 +161,7 @@ class TrackingController extends Controller
 
     public function iniciar_tracking()
     {
-        TrackingTemporal::truncate();
+        /*TrackingTemporal::truncate();
         $list_tracking = DB::connection('sqlsrv')->select('EXEC usp_ver_despachos_tracking ?', ['T']);
         foreach($list_tracking as $list){
             if($list->id_origen_hacia=="4" || $list->id_origen_hacia=="6"){
@@ -328,7 +328,7 @@ class TrackingController extends Controller
                 'iniciar' => 1,
                 'fec_act' => now()
             ]);
-        }
+        }*/
     }
 
     public function llegada_tienda()
