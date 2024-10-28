f1787817c957898568f47c239d4e66e6f6ce3f9e
Daniel Palomino <daniel11143118@gmail.com>
2024-10-28 08:52:13 -0500

    cambios

--- database/migrations/2024_05_07_081051_create_area_table.php
+++ database/migrations/2024_05_07_081051_create_area_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->integer('user_act')->nullable();
             $table->dateTime('fec_eli')->nullable();
             $table->integer('user_eli')->nullable();
+            $table->string('id_base')->nullable();
             $table->foreign('id_direccion','are_fk_id_dir')->references('id_direccion')->on('direccion');
             $table->foreign('id_gerencia','are_fk_id_ger')->references('id_gerencia')->on('gerencia');
             $table->foreign('id_departamento','are_fk_id_dep')->references('id_sub_gerencia')->on('sub_gerencia');
