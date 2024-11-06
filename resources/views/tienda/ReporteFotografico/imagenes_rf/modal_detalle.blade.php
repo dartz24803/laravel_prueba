3505bc5773e2f264714310c7c6a6aeed54d3c1f2
dartz24803 <pcardenasfuster@gmail.com>
2024-11-06 17:59:28 -0500

    reporte fotografico

--- resources/views/tienda/ReporteFotografico/imagenes_rf/modal_detalle.blade.php
+++ resources/views/tienda/ReporteFotografico/imagenes_rf/modal_detalle.blade.php
@@ -25,10 +25,7 @@
             <div class="d-flex justify-content-center align-items-center">
                 <div class="form-check">
                     <button class="btn btn-warning" value="90" name="orientation" id="rotateButton" onclick="Rotar_Imagen_RF()">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-rotate-cw">
-                            <polyline points="23 4 23 10 17 10"></polyline>
-                            <path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10"></path>
-                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-rotate-ccw"><polyline points="1 4 1 10 7 10"></polyline><path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path></svg>
                     </button>
                 </div>
             </div>
