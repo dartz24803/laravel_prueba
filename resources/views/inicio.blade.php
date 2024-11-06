d9a53bef4dd5287f989f429c6a2029777a37a34d
dartz24803 <pcardenasfuster@gmail.com>
2024-11-06 09:06:57 -0500

    inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -376,7 +376,7 @@
             /* Desactiva los eventos de puntero (clics) */
             opacity: 0.5;
             /* Cambia la opacidad para dar un efecto de deshabilitado */
-            cursor: not-allowed;
+            cursor: default;
             /* Cambia el cursor a uno que indique que no es interactivo */
         }
 
