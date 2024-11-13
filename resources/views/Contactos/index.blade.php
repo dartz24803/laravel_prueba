e30d38dfcd8d0a2e3eb96bf102e0029290001ea4
dartz24803 <pcardenasfuster@gmail.com>
2024-11-13 17:58:34 -0500

    contactos

--- resources/views/Contactos/index.blade.php
+++ resources/views/Contactos/index.blade.php
@@ -195,9 +195,18 @@
 
     }
 
-    hlservicior   
+    $(document).ready(function() {
 
+        checkall('contact-check-all', 'contact-chkbox');
 
+        $('#input-search').on('keyup', function() {
+        var rex = new RegExp($(this).val(), 'i');
+            $('.searchable-items .items:not(.items-header-section)').hide();
+            $('.searchable-items .items:not(.items-header-section)').filter(function() {
+                return rex.test($(this).text());
+            }).show();
+        });
+    });
 </script>
 
 @endsection
