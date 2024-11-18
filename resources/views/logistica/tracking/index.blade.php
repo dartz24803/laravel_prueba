219945924222c8349d0395d7a0047e5ef1b30697
dartz24803 <pcardenasfuster@gmail.com>
2024-11-18 18:06:38 -0500

    fix

--- resources/views/logistica/tracking/index.blade.php
+++ resources/views/logistica/tracking/index.blade.php
@@ -39,7 +39,7 @@
             $("#hlogisticas").attr('aria-expanded', 'true');
             $("#trackings").addClass('active');
 
-            Bd_Tracking();
+            Tracking();
         });
 
         function Tracking() {
