a093d5cc4364142ead666d350e1a9c32101415f1
dartz24803 <pcardenasfuster@gmail.com>
2024-11-25 16:56:29 -0500

    fix

--- resources/views/rrhh/Amonestacion/Emitidas/modal_registrar.blade.php
+++ resources/views/rrhh/Amonestacion/Emitidas/modal_registrar.blade.php
@@ -26,46 +26,46 @@
                     <input type="date" class="form-control" id="fecha" name="fecha" value="<?php echo date('Y-m-d') ?>">
                 </div>
             </div>
-            
-            <?php if(session('usuario')->id_nivel==2 || session('usuario')->id_nivel==1 || session('usuario')->id_puesto==133){?> 
+
+            <?php if(session('usuario')->id_nivel==2 || session('usuario')->id_nivel==1 || session('usuario')->id_puesto==133){?>
                 <div class="form-group col-md-8">
                     <label class="col-sm-12 control-label text-bold">Solicitante: </label>
                     <div class="col-md">
                         <!--<select class="form-control basic" name="id_solicitante" id="id_solicitante" onchange="Busca_Colaborador_Area('1')">-->
                         <select class="form-control basic" name="id_solicitante" id="id_solicitante" >
                             <option value="0">Seleccione</option>
-                            <?php foreach($list_responsables as $list){ ?> 
+                            <?php foreach($list_responsables as $list){ ?>
                                 <option value="<?php echo $list['id_usuario']; ?>"><?php echo $list['usuario_nombres']." ".$list['usuario_apater']." ".$list['usuario_amater'];?></option>
                             <?php }  ?>
                         </select>
                     </div>
-                </div>  
-            <?php if(session('usuario')->id_nivel==2 || session('usuario')->id_puesto==133){?> 
-                <input type="hidden" name="estado_amonestacion" id="estado_amonestacion" value="2">     
+                </div>
+            <?php if(session('usuario')->id_nivel==2 || session('usuario')->id_puesto==133){?>
+                <input type="hidden" name="estado_amonestacion" id="estado_amonestacion" value="2">
             <?php }else{?>
-                <input type="hidden" name="estado_amonestacion" id="estado_amonestacion" value="1">    
-            <?php }}else{?> 
-            <input type="hidden" name="id_solicitante" id="id_solicitante" value="<?php echo session('usuario')->id_usuario ?>">    
-            <input type="hidden" name="estado_amonestacion" id="estado_amonestacion" value="1">    
+                <input type="hidden" name="estado_amonestacion" id="estado_amonestacion" value="1">
+            <?php }}else{?>
+            <input type="hidden" name="id_solicitante" id="id_solicitante" value="<?php echo session('usuario')->id_usuario ?>">
+            <input type="hidden" name="estado_amonestacion" id="estado_amonestacion" value="1">
             <?php }?>
             <div class="form-group col-md-8">
                 <label class="col-sm-12 control-label text-bold">Colaborador: </label>
                 <div class="col-md" id="cmb_colaborador">
                     <select class="form-control multivalue_pps" name="id_usuario[]" id="id_usuario" multiple="multiple">
-                        <?php foreach($list_colaborador as $list){ ?> 
+                        <?php foreach($list_colaborador as $list){ ?>
                             <option value="<?php echo $list['id_usuario']; ?>"><?php echo $list['usuario_nombres']." ".$list['usuario_apater']." ".$list['usuario_amater'];?></option>
                         <?php }  ?>
                     </select>
                 </div>
-            </div> 
-            
+            </div>
+
             <div class="form-group col-md-4">
                 <label class="col-sm-12 control-label text-bold">Tipo: </label>
                 <div class="col">
                     <select name="tipo" id="tipo" class="form-control">
                         <option value="0">Seleccione</option>
-                        <?php foreach($list_tipo_amonestacion as $list){ ?> 
-                            <option value="<?php echo $list['id_tipo_amonestacion'] ?>"><?php echo $list['nom_tipo_amonestacion'] ?></option>     
+                        <?php foreach($list_tipo_amonestacion as $list){ ?>
+                            <option value="<?php echo $list['id_tipo_amonestacion'] ?>"><?php echo $list['nom_tipo_amonestacion'] ?></option>
                         <?php }?>
                     </select>
                 </div>
@@ -76,8 +76,8 @@
                 <div class="col">
                     <select name="id_gravedad_amonestacion" id="id_gravedad_amonestacion" class="form-control"><!-- onchange="Cambio_Motivo_Gravedad('1')"-->
                         <option value="0">Seleccione</option>
-                        <?php foreach($list_gravedad_amonestacion as$list){ ?> 
-                        <option value="<?php echo $list['id_gravedad_amonestacion'] ?>"><?php echo $list['nom_gravedad_amonestacion'] ?></option>    
+                        <?php foreach($list_gravedad_amonestacion as$list){ ?>
+                        <option value="<?php echo $list['id_gravedad_amonestacion'] ?>"><?php echo $list['nom_gravedad_amonestacion'] ?></option>
                         <?php }?>
                     </select>
                 </div>
@@ -88,8 +88,8 @@
                 <div class="col" id="cmb_motivo">
                     <select name="motivo" id="motivo" class="form-control">
                         <option value="0">Seleccione</option>
-                        <?php foreach($list_motivo_amonestacion as $list){ ?> 
-                            <option value="<?php echo $list['id_motivo_amonestacion'] ?>"><?php echo $list['nom_motivo_amonestacion'] ?></option>    
+                        <?php foreach($list_motivo_amonestacion as $list){ ?>
+                            <option value="<?php echo $list['id_motivo_amonestacion'] ?>"><?php echo $list['nom_motivo_amonestacion'] ?></option>
                         <?php }?>
                     </select>
                 </div>
@@ -100,7 +100,7 @@
                 <div class="col">
                     <textarea class="form-control" name="detalle" id="detalle" rows="3" placeholder="Ingresar Detalle del suceso"></textarea>
                 </div>
-            </div> 
+            </div>
         </div>
     </div>
 
@@ -120,7 +120,7 @@
     var ss = $(".multivalue_pps").select2({
         tags: true
     });
-    
+
     $('.multivalue_pps').select2({
         dropdownParent: $('#ModalRegistro')
     });
@@ -171,17 +171,17 @@
                         ).then(function() {
                             $("#ModalRegistro .close").click()
                             Lista_Amonestaciones_Emitidas();
-                        });  
+                        });
                     }
-                }
-            });
-        }else{
-            bootbox.alert(msgDate)
-            var input = $(inputFocus).parent();
-            $(input).addClass("has-error");
-            $(input).on("change", function() {
-                if ($(input).hasClass("has-error")) {
-                    $(input).removeClass("has-error");
+                },
+                error: function(xhr) {
+                    var errors = xhr.responseJSON.errors;
+                    var firstError = Object.values(errors)[0][0];
+                    Swal.fire(
+                        '¡Ups!',
+                        firstError,
+                        'warning'
+                    );
                 }
             });
         }
@@ -194,55 +194,86 @@
         }
         if ($('#fecha'+v).val() === '') {
             msgDate = 'Debe ingresar fecha.';
-            inputFocus = '#fecha'+v;
+            Swal.fire(
+                '¡Ups!',
+                msgDate,
+                'warning'
+            );
             return false;
         }
         if(t==2){
             if($('#id_usuario'+v).val() == '0') {
                 msgDate = 'Debe seleccionar colaborador';
-                inputFocus = '#id_usuario'+v;
+                Swal.fire(
+                    '¡Ups!',
+                    msgDate,
+                    'warning'
+                );
                 return false;
             }
         }else{
             if($('#id_usuario'+v).val() == '') {
                 msgDate = 'Debe seleccionar al menos un colaborador';
-                inputFocus = '#id_usuario'+v;
+                Swal.fire(
+                    '¡Ups!',
+                    msgDate,
+                    'warning'
+                );
                 return false;
             }
         }
         if($('#nivel'+v).val()==2){
             if($('#id_solicitante'+v).val() == '0') {
                 msgDate = 'Debe seleccionar solicitante';
-                inputFocus = '#id_solicitante'+v;
+                Swal.fire(
+                    '¡Ups!',
+                    msgDate,
+                    'warning'
+                );
                 return false;
             }
         }
-        
+
         if($('#tipo'+v).val() == '') {
             msgDate = 'Debe ingresar tipo de amonestación';
-            inputFocus = '#tipo'+v;
+            Swal.fire(
+                '¡Ups!',
+                msgDate,
+                'warning'
+            );
             return false;
         }
         if($('#id_gravedad_amonestacion'+v).val() == '') {
             msgDate = 'Debe seleccionar gravedad de amonestación';
-            inputFocus = '#id_gravedad_amonestacion'+v;
+            Swal.fire(
+                '¡Ups!',
+                msgDate,
+                'warning'
+            );
             return false;
         }
         if($('#motivo'+v).val() == '') {
             msgDate = 'Debe ingresar motivo de amonestación';
-            inputFocus = '#motivo'+v;
+            Swal.fire(
+                '¡Ups!',
+                msgDate,
+                'warning'
+            );
             return false;
         }
         if(t==2){
             if($('#documento').val() != ''){
                 if($('#estado_amonestacion').val() == '0') {
                     msgDate = 'Debe seleccionar estado';
-                    inputFocus = '#estado_amonestacion';
+                    Swal.fire(
+                        '¡Ups!',
+                        msgDate,
+                        'warning'
+                    );
                     return false;
                 }
-            }  
+            }
         }
-        
 
         return true;
     }
