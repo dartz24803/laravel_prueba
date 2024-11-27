c80e2da5143fce241ade6b42e0b8d281fb2d835d
dartz24803 <pcardenasfuster@gmail.com>
2024-11-27 15:28:10 -0500

    fix

--- resources/views/logistica/tracking/bd_tracking/lista.blade.php
+++ resources/views/logistica/tracking/bd_tracking/lista.blade.php
@@ -79,9 +79,9 @@ $(document).ready(function() {
     $('#tabla_js thead').on('click', 'th', function() {
         if ($(this).attr('id') !== 'ordenar-fechas') {
             $('#tabla_js thead th .orden-icono').html(`
-                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="#231b2e4b" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="#231b2e4b" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
-                        `);
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="#231b2e4b" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="#231b2e4b" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+            `);
         }
     });
 });
