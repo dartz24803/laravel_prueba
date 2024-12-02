fd05ed08cb534fe88b4b2a5d8bbb972450861142
dartz24803 <pcardenasfuster@gmail.com>
2024-12-02 08:44:32 -0500

    fecha

--- resources/views/tienda/administrador/supervision_tienda/lista.blade.php
+++ resources/views/tienda/administrador/supervision_tienda/lista.blade.php
@@ -15,15 +15,15 @@
             <tr class="text-center">
                 <td>{{ $list->orden }}</td>
                 <td>{{ $list->base }}</td>
-                <td>{{ $list->fecha }}</td>
+                <td data-order="{{ $list->orden }}">{{ $list->fecha }}</td>
                 <td>
                     <span class="badge badge-{{ $list->color_estado }}">{{ $list->nom_estado }}</span>
                 </td>
                 <td class="text-left">{{ $list->observacion }}</td>
                 <td>
                     @if ($list->v_evidencia>0)
-                        <a href="javascript:void(0);" data-toggle="modal" 
-                        data-target="#ModalUpdate" 
+                        <a href="javascript:void(0);" data-toggle="modal"
+                        data-target="#ModalUpdate"
                         app_elim="{{ route('administrador_st.evidencia', $list->id) }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye text-success">
                                 <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
@@ -33,25 +33,25 @@
                     @endif
                 </td>
                 <td class="text-center">
-                    <div class="btn-group dropleft" role="group"> 
+                    <div class="btn-group dropleft" role="group">
                         <a id="btnDropLeft" type="button" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>
                         </a>
                         <div class="dropdown-menu" aria-labelledby="btnDropLeft" style="padding:0;">
-                            <a href="javascript:void(0);" class="dropdown-item" data-toggle="modal" 
-                            data-target="#ModalUpdate" 
+                            <a href="javascript:void(0);" class="dropdown-item" data-toggle="modal"
+                            data-target="#ModalUpdate"
                             app_elim="{{ route('administrador_st.show', $list->id) }}">
                                 Ver
                             </a>
                             @if (session('usuario')->id_puesto!=311)
                                 @if (date('Y-m-d')==$list->orden)
-                                    <a href="javascript:void(0);" class="dropdown-item" data-toggle="modal" 
-                                    data-target="#ModalUpdateGrande" 
+                                    <a href="javascript:void(0);" class="dropdown-item" data-toggle="modal"
+                                    data-target="#ModalUpdateGrande"
                                     app_upd_grande="{{ route('administrador_st.edit', $list->id) }}">
                                         Editar
                                     </a>
 
-                                    <a href="javascript:void(0);" class="dropdown-item" 
+                                    <a href="javascript:void(0);" class="dropdown-item"
                                     onclick="Delete_Supervision_Tienda('{{ $list->id }}')">
                                         Eliminar
                                     </a>
@@ -60,21 +60,21 @@
                         </div>
                     </div>
                 </td>
-            </tr>   
+            </tr>
         @endforeach
     </tbody>
 </table>
 
 <script>
     $(document).ready(function() {
-        @if (session('usuario')->id_nivel==1 || session('usuario')->id_puesto==6 || 
-        session('usuario')->id_puesto==12 || session('usuario')->id_puesto==19 || session('usuario')->id_puesto==21 || 
-        session('usuario')->id_puesto==23 || session('usuario')->id_puesto==38 || 
-        session('usuario')->id_puesto==81 || session('usuario')->id_puesto==111 || 
-        session('usuario')->id_puesto==122 || session('usuario')->id_puesto==137 || 
-        session('usuario')->id_puesto==164 || session('usuario')->id_puesto==158 || 
-        session('usuario')->id_puesto==9 || session('usuario')->id_puesto==128 || 
-        session('usuario')->id_puesto==27 || session('usuario')->id_puesto==10 || 
+        @if (session('usuario')->id_nivel==1 || session('usuario')->id_puesto==6 ||
+        session('usuario')->id_puesto==12 || session('usuario')->id_puesto==19 || session('usuario')->id_puesto==21 ||
+        session('usuario')->id_puesto==23 || session('usuario')->id_puesto==38 ||
+        session('usuario')->id_puesto==81 || session('usuario')->id_puesto==111 ||
+        session('usuario')->id_puesto==122 || session('usuario')->id_puesto==137 ||
+        session('usuario')->id_puesto==164 || session('usuario')->id_puesto==158 ||
+        session('usuario')->id_puesto==9 || session('usuario')->id_puesto==128 ||
+        session('usuario')->id_puesto==27 || session('usuario')->id_puesto==10 ||
         session('usuario')->id_puesto==55)
             $('#tabla_js').DataTable({
                 "dom": "<'dt--top-section'<'row'<'col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center'l><'col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center mt-sm-0 mt-3'f>>>" +
@@ -92,11 +92,11 @@
                 "stripeClasses": [],
                 "lengthMenu": [10, 20, 50],
                 "pageLength": 10,
-                "aoColumnDefs" : [ 
+                "aoColumnDefs" : [
                     {
                         'targets' : [ 0 ],
                         'visible' : false
-                    } 
+                    }
                 ]
             });
         @else
@@ -116,11 +116,11 @@
                 "stripeClasses": [],
                 "lengthMenu": [10, 20, 50],
                 "pageLength": 10,
-                "aoColumnDefs" : [ 
+                "aoColumnDefs" : [
                     {
                         'targets' : [ 0,1 ],
                         'visible' : false
-                    } 
+                    }
                 ]
             });
         @endif
