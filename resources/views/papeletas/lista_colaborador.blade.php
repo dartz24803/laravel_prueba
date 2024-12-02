9c8a39905ff3e4e74748e688672839c3c332a9e2
dartz24803 <pcardenasfuster@gmail.com>
2024-12-02 15:35:35 -0500

    papeletas

--- resources/views/papeletas/lista_colaborador.blade.php
+++ resources/views/papeletas/lista_colaborador.blade.php
@@ -13,16 +13,16 @@
         </tr>
     </thead>
     <tbody>
-    <?php foreach($list_papeletas_salida as $list) {  ?>   
+    <?php foreach($list_papeletas_salida as $list) {  ?>
         <tr>
             <td>
-                <?php 
+                <?php
                     if( $list['id_motivo']==1){
-                        echo "Laboral"; 
+                        echo "Laboral";
                     }else if ($list['id_motivo']==2){
-                        echo "Personal"; 
+                        echo "Personal";
                     }else{
-                        echo $list['motivo']; 
+                        echo $list['motivo'];
                     }
                 ?>
             </td>
@@ -35,59 +35,59 @@
             <td>
                 <?php
                     echo date_format(date_create($list['fec_solicitud']), "d/m/Y");
-                ?>     
+                ?>
             </td>
             <td align="center">
                 <?php
                     if($list['sin_ingreso'] == 1 ){
                         echo "Sin Ingreso";
                     }else{
-                        echo $list['hora_salida']; 
+                        echo $list['hora_salida'];
                     }
                 ?>
             </td>
             <td align="center">
-                <?php 
+                <?php
                     if($list['sin_retorno'] == 1 ){
                         echo "Sin Retorno";
-                    }else{ 
-                        echo $list['hora_retorno'];
-                    } 
-                ?>
-            </td>
-
-            <td > 
-                <?php 
-                    if( $list['estado_solicitud']=='1'){
-                        echo "<span class='shadow-none badge badge-warning'>En proceso</span>"; 
-                    }else if ($list['estado_solicitud']=='2'){
-                        echo "<span class='shadow-none badge badge-primary'>Aprobado</span>"; 
-                    }else if ($list['estado_solicitud']=='3'){
-                        echo " <span class='shadow-none badge badge-danger'>Denegado</span>"; 
-                    }else if ($list['estado_solicitud']=='4'){
-                        echo "<span class='shadow-none badge badge-warning'>En proceso - Aprobación Gerencia</span>"; 
-                    }else if ($list['estado_solicitud']=='5'){
-                        echo "<span class='shadow-none badge badge-warning'>En proceso - aprobación RRHH</span>"; 
                     }else{
-                        echo "<span class='shadow-none badge badge-primary'>Error</span>"; 
+                        echo $list['hora_retorno'];
                     }
                 ?>
             </td>
-            
+
+            <td >
+                <?php
+                    if( $list['estado_solicitud']=='1'){
+                        echo "<span class='shadow-none badge badge-warning'>En proceso</span>";
+                    }else if ($list['estado_solicitud']=='2'){
+                        echo "<span class='shadow-none badge badge-primary'>Aprobado</span>";
+                    }else if ($list['estado_solicitud']=='3'){
+                        echo " <span class='shadow-none badge badge-danger'>Denegado</span>";
+                    }else if ($list['estado_solicitud']=='4'){
+                        echo "<span class='shadow-none badge badge-warning'>En proceso - Aprobación Gerencia</span>";
+                    }else if ($list['estado_solicitud']=='5'){
+                        echo "<span class='shadow-none badge badge-warning'>En proceso - aprobación RRHH</span>";
+                    }else{
+                        echo "<span class='shadow-none badge badge-primary'>Error</span>";
+                    }
+                ?>
+            </td>
+
             <td class="text-center">
 
 
                 <?php if( $list['estado_solicitud']=='1'){ ?>
-                    <a href="javascript:void(0);" title="Editar" data-toggle="modal" data-target="#ModalUpdateSlide" app_upd_slide="{{ url('Papeletas/Modal_Update_Papeletas_Salida/'. $list['id_solicitudes_user']); }}">
+                    <!-- <a href="javascript:void(0);" title="Editar" data-toggle="modal" data-target="#ModalUpdateSlide" app_upd_slide="{{ url('Papeletas/Modal_Update_Papeletas_Salida/'. $list['id_solicitudes_user']); }}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 p-1 br-6 mb-1"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
-                    </a>
+                    </a> -->
                     <a href="#" class="" title="Eliminar" onclick="Delete_Papeletas_Salida('<?php echo $list['id_solicitudes_user']; ?>')" id="Eliminar" role="button">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 text-danger">
                             <polyline points="3 6 5 6 21 6"></polyline>
                             <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
                             <line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line>
                         </svg>
-                    </a> 
+                    </a>
                 <?php  }else if ($list['estado_solicitud']=='2'){?>
                     <a title="No puedes editar" class="anchor-tooltip tooltiped"><div class="divdea">
                     <svg id="Layer_1" width="13" height="13" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><defs><style>.cls-1{fill:#2d3e50;}</style></defs><title>n</title><path class="cls-1" d="M86.15787,99.25657c-3.54161,2.827-10.03158,6.41724-14.75995,6.08384-4.67736-.3298-3.78182-4.78987-2.85481-8.295l7.83763-29.63476a13.29171,13.29171,0,0,0-25.68221-6.86278C49.55418,64.7858,40.39666,102.57942,40.34023,102.816c-1.28065,5.36943-2.81226,12.2324-.45115,17.525,3.58188,8.02819,14.46035,5.69646,21.06968,3.78541a52.68574,52.68574,0,0,0,12.91952-5.64322,118.52775,118.52775,0,0,0,13.15678-10.41187Z"/><path class="cls-1" d="M74.55393,2.049c-9.8517-.61753-19.65075,8.23893-20.034,18.3877a15.14774,15.14774,0,0,0,2.23531,8.54311c6.11649,9.89677,20.16846,7.7415,27.76526.91074C94.54734,20.87483,87.832,2.88134,74.55393,2.049Z"/></svg>
