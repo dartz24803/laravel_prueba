a51f50782eef9aa3580049a066d7d4071359998f
Bryan Hector Rafael Andia <brafael@lanumero1.local>
2024-12-04 11:38:46 -0500

    scroll horizontal reportenew

--- resources/views/reportenew/indexp.blade.php
+++ resources/views/reportenew/indexp.blade.php
@@ -6,6 +6,11 @@
 
 @section('content')
 <style>
+    .scroll-item:hover {
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+        border-radius: 10px;
+    }
+
     #cancel-row {
         height: calc(100vh - 100px);
         display: flex;
@@ -59,13 +64,43 @@
                 <div class="statbox widget box box-shadow" style="height: 100%;">
                     <div class="widget-content widget-content-area simple-tab" style="height: 100%;">
                         @if (count($list_reportes) > 4)
-                        <select class="form-control mt-4 ml-2" id="reporteSelect"
-                            onchange="showIframe(this.value, this);">
-                            <option value="">Seleccione un reporte</option>
+                        <div class="scroll-container" style="display: flex; overflow-x: auto; padding: 10px 0;">
                             @foreach ($list_reportes as $reporte)
-                            <option value="{{ $reporte->iframe }}">{{ $reporte->nom_bi }}</option>
+                            <div class="scroll-item" style="min-width: 200px; margin-right: 15px; text-align: center; transition: box-shadow 0.3s ease;">
+                                <div style="display: flex; justify-content: space-between; align-items: center; width: 100%; flex-wrap: nowrap; border: 0.5px solid #fea701; border-radius: 8px;">
+                                    <!-- Nombre del reporte con truncado y ... si se colapsa -->
+                                    <div class="scroll-item-text" style="flex-grow: 0; width: 80%; display: flex; align-items: center; ">
+                                        <div id="btn-{{ $reporte->id }}" class="btn-link"
+                                            onclick="showIframe('{{ $reporte->iframe }}', this);"
+                                            style="white-space: nowrap; text-overflow: ellipsis; overflow: hidden; background-color: white; color: #000; cursor: pointer; margin: 4px; padding: 4px; ">
+                                            {{ $reporte->nom_bi }}
+                                        </div>
+
+                                    </div>
+                                    <!-- Lupa -->
+                                    <a title="{{ $reporte->nom_bi }}"
+                                        onclick="showImageModal('{{ $reporte->img1 }}', '{{ $reporte->img2 }}', '{{ $reporte->img3 }}')"
+                                        data-toggle="modal" data-target="#imageModal"
+                                        style="display: flex; align-items: center; justify-content: center; padding-left: 10px; width: 30%; max-width: 50px;">
+                                        <svg version="1.1" id="Capa_1" style="width:20px; height:20px; cursor:pointer;"
+                                            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                            viewBox="0 0 512.81 512.81" style="enable-background:new 0 0 512.81 512.81;" xml:space="preserve">
+                                            <rect x="260.758" y="276.339" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -125.9193 303.0804)"
+                                                style="fill:#344A5E;" width="84.266" height="54.399" />
+                                            <circle style="fill:#8AD7F8;" cx="174.933" cy="175.261" r="156.8" />
+                                            <path style="fill:#415A6B;"
+                                                d="M299.733,300.061c-68.267,68.267-180.267,68.267-248.533,0s-68.267-180.267,0-248.533s180.267-68.267,248.533,0S368,231.794,299.733,300.061z M77.867,78.194c-53.333,53.333-53.333,141.867,0,195.2s141.867,53.333,195.2,0s53.333-141.867,0-195.2S131.2,23.794,77.867,78.194z" />
+                                            <path style="fill:#F05540;"
+                                                d="M372.267,286.194c-7.467-7.467-19.2-7.467-26.667,0l-59.733,59.733c-7.467,7.467-7.467,19.2,0,26.667s19.2,7.467,26.667,0l59.733-59.733C379.733,305.394,379.733,293.661,372.267,286.194z" />
+                                            <path style="fill:#F3705A;"
+                                                d="M410.667,496.328C344.533,436.594,313.6,372.594,313.6,372.594l59.733-59.733c0,0,65.067,32,123.733,97.067c21.333,24.533,21.333,60.8-2.133,84.267l0,0C471.467,517.661,434.133,518.728,410.667,496.328z" />
+                                        </svg>
+                                    </a>
+                                </div>
+
+                            </div>
                             @endforeach
-                        </select>
+                        </div>
                         @else
                         <ul class="nav nav-tabs mt-4 ml-2" id="simpletab" role="tablist">
                             @foreach ($list_reportes as $index => $reporte)
