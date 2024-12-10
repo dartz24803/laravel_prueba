dc605ec01371219993a5ae6edfdbeb6ed90b2a67
dartz24803 <pcardenasfuster@gmail.com>
2024-12-10 15:46:32 -0500

    amonestaciones

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -635,10 +635,7 @@
                     <li class="menu" id="mamonestacion">
                         <a href="{{ url('Amonestaciones/Amonestaciones_Recibidas_Index') }}" id="hamonestacion" class="dropdown-toggle" data-toggle="tooltip" data-placement="right" data-html="true" title="• Recibidas">
                             <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
-                                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-meh"><circle cx="12" cy="12" r="10"></circle><line x1="8" y1="15" x2="16" y2="15"></line><line x1="9" y1="9" x2="9.01" y2="9"></line><line x1="15" y1="9" x2="15.01" y2="9"></line></svg>
                                 <span id="icono_active"></span>
                                 <span> Amonestaciones</span>
                             </div>
