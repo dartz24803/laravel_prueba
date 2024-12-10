34afd532985f41759e8513596e55f0426cccdbf8
Bryan Hector Rafael Andia <brafael@lanumero1.local>
2024-12-10 10:53:20 -0500

    validaciones de soporte - switches

--- resources/views/finanzas/contabilidad/facturacion/lista.blade.php
+++ resources/views/finanzas/contabilidad/facturacion/lista.blade.php
@@ -405,7 +405,7 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             },
-            "lengthMenu": [250],
+            "lengthMenu": [800],
             "order": [
                 [0, "asc"]
             ],
@@ -500,7 +500,6 @@
                 "sSearchPlaceholder": "Buscar...",
                 "sLengthMenu": "Resultados :  _MENU_",
             },
-
             "scrollCollapse": true,
             "scrollX": true,
             "scrollY": 400,
@@ -824,7 +823,7 @@
                                             "targets": 6
                                         }
                                     ],
-                                    "lengthMenu": [250],
+                                    "lengthMenu": [800],
                                     "oLanguage": {
                                         "oPaginate": {
                                             "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
--- resources/views/soporte/soporte_master/lista.blade.php
+++ resources/views/soporte/soporte_master/lista.blade.php
@@ -115,33 +115,57 @@
 </table>
 
 <script>
-    $(document).ready(function() {
-        $('#tabla_js').DataTable({
-            "dom": "<'dt--top-section'<'row'<'col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center'l><'col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center mt-sm-0 mt-3'f>>>" +
-                "<'table-responsive'tr>" +
-                "<'dt--bottom-section d-sm-flex justify-content-sm-between text-center'<'dt--pages-count  mb-sm-0 mb-3'i><'dt--pagination'p>>",
-            responsive: true,
-            order: [
-                [0, "desc"]
-            ],
-            "oLanguage": {
-                "oPaginate": {
-                    "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
-                    "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
-                },
-                "sInfo": "Mostrando página _PAGE_ de _PAGES_",
-                "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
-                "sSearchPlaceholder": "Buscar...",
-                "sLengthMenu": "Resultados :  _MENU_",
-                "sEmptyTable": "No hay datos disponibles en la tabla",
+    var tabla = $('#tabla_js').DataTable({
+        "dom": "<'dt--top-section'<'row'<'col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center'l><'col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center mt-sm-0 mt-3'f>>>" +
+            "<'table-responsive'tr>" +
+            "<'dt--bottom-section d-sm-flex justify-content-sm-between text-center'<'dt--pages-count  mb-sm-0 mb-3'i><'dt--pagination'p>>",
+        responsive: true,
+        order: [
+            [0, "desc"]
+        ],
+        "oLanguage": {
+            "oPaginate": {
+                "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
+                "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
             },
-            "stripeClasses": [],
-            "lengthMenu": [10, 20, 50],
-            "pageLength": 10,
-            "columnDefs": [{
-                'targets': 0, // Índice de la columna que quieres ocultar
-                'visible': false // Oculta la columna
-            }],
-        });
+            "sInfo": "Mostrando página _PAGE_ de _PAGES_",
+            "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+            "sSearchPlaceholder": "Buscar...",
+            "sLengthMenu": "Resultados :  _MENU_",
+            "sEmptyTable": "No hay datos disponibles en la tabla",
+        },
+        "stripeClasses": [],
+        "lengthMenu": [10, 20, 50],
+        "pageLength": 10,
+        "columnDefs": [{
+            'targets': 0, // Índice de la columna que quieres ocultar
+            'visible': false // Oculta la columna
+        }],
+    });
+
+    $('#toggle-fr').change(function() {
+        var columnIndex = 3;
+        var visible = this.checked;
+        tabla.column(columnIndex).visible(visible);
+    });
+    $('#toggle-tipo').change(function() {
+        var columnIndex = 5;
+        var visible = this.checked;
+        tabla.column(columnIndex).visible(visible);
+    });
+    $('#toggle-esp').change(function() {
+        var columnIndex = 6;
+        var visible = this.checked;
+        tabla.column(columnIndex).visible(visible);
+    });
+    $('#toggle-ele').change(function() {
+        var columnIndex = 7;
+        var visible = this.checked;
+        tabla.column(columnIndex).visible(visible);
+    });
+    $('#toggle-res').change(function() {
+        var columnIndex = 9;
+        var visible = this.checked;
+        tabla.column(columnIndex).visible(visible);
     });
 </script>
