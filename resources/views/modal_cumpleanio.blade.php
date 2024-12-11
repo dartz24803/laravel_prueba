f91577ef53e41b39875f998d52c8418248982efc
dartz24803 <pcardenasfuster@gmail.com>
2024-12-11 15:55:51 -0500

    cumpleaños

--- resources/views/modal_cumpleanio.blade.php
+++ resources/views/modal_cumpleanio.blade.php
@@ -1,12 +1,11 @@
 
 <div class="modal-header" style="background-color: #add8e6ed;">
-    <h5 class="modal-title">Feliz Cumpleañoooos!! 🎈🎊🥳🎉🎉🍾🎊🎈</h5>
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-    </button>
+    <div class="col-md-12 row">
+        <h5 class="modal-title col-md-12 text-center">Feliz Cumpleañoooos!! 🎈🎊🥳🎉🎉🍾🎊🎈</h5>
+    </div>
 </div> 
 
-<div class="modal-body" style="max-height:450px; overflow:auto; background-color: #add8e6ed;">
+<div class="modal-body" style="max-height:565px; overflow:auto; background-color: #add8e6ed;">
     <div class="col-md-12 row">
         <div class="form-group col-md-12 text-center contenedor_gif">
             <img class="fondo-imagen" style="max-width:100%;" src="https://lanumerounocloud.com/intranet/PERFIL/Saludo_Temporal/{{ $get_id->archivo }}">
@@ -16,9 +15,6 @@
 </div>
 
 <style>
-    .modal-body{
-        max-height: 565px !important;
-    }
     .contenedor_gif {
         position: relative; /* Crea un contexto de posición */
     }
@@ -42,3 +38,13 @@
         pointer-events: none; /* Permite interacción con elementos debajo del GIF */
     }
 </style>
+<script>
+    $(document).ready(function() {
+        $(document).on('click', function(event) {
+            // Verificar si el clic está fuera del modal
+            if (!$(event.target).closest('.modal-content').length && $('.modal.show').length) {
+                $('#ModalUpdateSlide').modal('hide'); // Cierra el modal
+            }
+        });
+    });
+</script>
