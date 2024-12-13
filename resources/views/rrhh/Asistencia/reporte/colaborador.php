8924acc98aa45b15b216f0bc5002ee7f1659cff1
dartz24803 <pcardenasfuster@gmail.com>
2024-12-13 12:25:22 -0500

    asistencia

--- resources/views/rrhh/Asistencia/reporte/colaborador.php
+++ resources/views/rrhh/Asistencia/reporte/colaborador.php
@@ -1,4 +1,4 @@
 <option value="0">TODOS</option>
 <?php foreach($list_colaborador as $list){?> 
-    <option value="<?= $list['num_doc']; ?>"><?= $list['usuario_apater'].' '.$list['usuario_amater'].' '.$list['usuario_nombres']; ?></option>
+    <option value="<?= $list['id_usuario']; ?>"><?= $list['usuario_apater'].' '.$list['usuario_amater'].' '.$list['usuario_nombres']; ?></option>
 <?php } ?>
