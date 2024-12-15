b2b8ecf191d8077148451ef3b0dbec757d68b69b
dartz24803 <pcardenasfuster@gmail.com>
2024-12-15 17:26:54 -0500

    requerimiento surtido

--- app/Http/Controllers/RequerimientoSurtidoController.php
+++ app/Http/Controllers/RequerimientoSurtidoController.php
@@ -81,6 +81,7 @@ class RequerimientoSurtidoController extends Controller
             'mes' => 'Debe escoger mes.',
             'drequerimiento' => 'Debe adjuntar documento',
         ]);
+        $data['anio'] = $this->input->post("anio");
         $dato['anio'] = $this->input->post("anio");
         $dato['semana'] = $this->input->post("semana");
         $path = $_FILES["drequerimiento"]["tmp_name"];
@@ -144,10 +145,25 @@ class RequerimientoSurtidoController extends Controller
             $dato['B18'] = $hojaDeProductos->getCell('AK' . $indiceFila)->getValue();
             $dato['B19'] = $hojaDeProductos->getCell('AL' . $indiceFila)->getValue();
             $dato['B20'] = $hojaDeProductos->getCell('AM' . $indiceFila)->getValue();
+            $dato['B21'] = $hojaDeProductos->getCell('AN' . $indiceFila)->getValue();
 
             # Continúa con BEC y RN
-            $dato['BEC'] = $hojaDeProductos->getCell('AN' . $indiceFila)->getValue();
-            $dato['RN'] = strtoupper($hojaDeProductos->getCell('AO' . $indiceFila)->getValue());
+            $dato['BEC'] = $hojaDeProductos->getCell('AO' . $indiceFila)->getValue();
+            $dato['RN'] = strtoupper($hojaDeProductos->getCell('AP' . $indiceFila)->getValue());
+
+            if($dato['empresa']=="" && $dato['costo']=="" && $dato['pc']=="" && $dato['pv']=="" &&
+            $dato['pp']=="" && $dato['pc_b4']=="" && $dato['pv_b4']=="" && $dato['pp_b4']=="" &&
+            $dato['tipo_usuario']=="" && $dato['tipo_prenda']=="" && $dato['codigo_barra']=="" &&
+            $dato['autogenerado']=="" && $dato['estilo']=="" && $dato['decripcion']=="" &&
+            $dato['color']=="" && $dato['talla']=="" && $dato['stock']=="" && $dato['B01']=="" &&
+            $dato['B02']=="" && $dato['B03']=="" && $dato['B04']=="" && $dato['B05']=="" &&
+            $dato['B06']=="" && $dato['B07']=="" && $dato['B08']=="" && $dato['B09']=="" &&
+            $dato['B10']=="" && $dato['B11']=="" && $dato['B12']=="" && $dato['B13']=="" &&
+            $dato['B14']=="" && $dato['B15']=="" && $dato['B16']=="" && $dato['B17']=="" &&
+            $dato['B18']=="" && $dato['B19']=="" && $dato['B20']=="" && $dato['B21']=="" &&
+            $dato['BEC']=="" && $dato['RN']==""){
+                break;
+            }
 
             if (
                 substr($dato['tipo_usuario'], 0, 1) != "=" && $dato['tipo_usuario'] != "" &&
@@ -168,31 +184,30 @@ class RequerimientoSurtidoController extends Controller
                 substr($dato['B16'], 0, 1) != "=" && substr($dato['B17'], 0, 1) != "=" &&
                 substr($dato['B12'], 0, 1) != "=" && substr($dato['BEC'], 0, 1) != "=" &&
                 substr($dato['B19'], 0, 1) != "=" && substr($dato['B20'], 0, 1) != "=" &&
-                substr($dato['B18'], 0, 1) != "=" && substr($dato['RN'], 0, 1) != ""   &&
-                strlen($dato['RN']) < 2
+                substr($dato['B18'], 0, 1) != "=" && substr($dato['B21'], 0, 1) != "=" &&
+                substr($dato['RN'], 0, 1) != ""   && strlen($dato['RN']) < 2
             ) {
-
-
                 $total = $total + intval(strval($dato['B01'])) + intval(strval($dato['B02'])) +
-                    intval(strval($dato['B03'])) + intval(strval($dato['B04'])) +
-                    intval(strval($dato['B05'])) + intval(strval($dato['B06'])) +
-                    intval(strval($dato['B07'])) + intval(strval($dato['B08'])) +
-                    intval(strval($dato['B09'])) + intval(strval($dato['B10'])) +
-                    intval(strval($dato['B11'])) + intval(strval($dato['B13'])) + intval(strval($dato['B12'])) +
-                    intval(strval($dato['B14'])) + intval(strval($dato['B15'])) + intval(strval($dato['BEC'])) +
-                    intval(strval($dato['B16'])) + intval(strval($dato['B17'])) + intval(strval($dato['B18'])) +
-                    intval(strval($dato['B19'])) + intval(strval($dato['B20']));
+                        intval(strval($dato['B03'])) + intval(strval($dato['B04'])) +
+                        intval(strval($dato['B05'])) + intval(strval($dato['B06'])) +
+                        intval(strval($dato['B07'])) + intval(strval($dato['B08'])) +
+                        intval(strval($dato['B09'])) + intval(strval($dato['B10'])) +
+                        intval(strval($dato['B11'])) + intval(strval($dato['B13'])) +
+                        intval(strval($dato['B12'])) + intval(strval($dato['B14'])) +
+                        intval(strval($dato['B15'])) + intval(strval($dato['BEC'])) +
+                        intval(strval($dato['B16'])) + intval(strval($dato['B17'])) +
+                        intval(strval($dato['B18'])) + intval(strval($dato['B19'])) +
+                        intval(strval($dato['B20'])) + intval(strval($dato['B21']));
                 $list_duplicados = DB::connection('sqlsrv')
                     ->table('dpedido_lnuno')
                     ->where('codigo_barra', $dato['codigo_barra'])
                     ->where('semana', $dato['semana'])
                     ->where('anio', $dato['anio'])
                     ->where('estado', 1)
-                    ->get();
-                // print_r($list_duplicados);
+                    ->exists();
                 $dato['caracter'] = "";
 
-                if ((trim($dato['RN']) !== 'N' && trim($dato['RN']) !== 'R' && trim($dato['RN']) !== 'T') || $list_duplicados->isEmpty()) {
+                if ((trim($dato['RN']) !== 'N' && trim($dato['RN']) !== 'R' && trim($dato['RN']) !== 'T') || $list_duplicados) {
                     $duplicados = $duplicados + 1;
                     if (trim($dato['RN']) !== 'N' && trim($dato['RN']) !== 'R' && trim($dato['RN']) !== 'T') {
                         $dato['caracter'] = "Caracter no permitido:" . $dato['RN'];
@@ -226,7 +241,8 @@ class RequerimientoSurtidoController extends Controller
                     substr($dato['B16'], 0, 1) == "=" || substr($dato['B17'], 0, 1) == "=" ||
                     substr($dato['B12'], 0, 1) == "=" || substr($dato['BEC'], 0, 1) == "=" ||
                     substr($dato['B19'], 0, 1) == "=" || substr($dato['B20'], 0, 1) == "=" ||
-                    substr($dato['B18'], 0, 1) == "=" || substr($dato['RN'], 0, 1) == ""
+                    substr($dato['B18'], 0, 1) == "=" || substr($dato['B21'], 0, 1) == "=" ||
+                    substr($dato['RN'], 0, 1) == ""
                 ) {
                     $columna = "";
                     if (substr($dato['tipo_usuario'], 0, 1) == "=" || $dato['tipo_usuario'] == "") {
@@ -316,8 +332,11 @@ class RequerimientoSurtidoController extends Controller
                     if (substr($dato['B20'], 0, 1) == "=") {
                         $columna = $columna . "AM,";
                     }
+                    if (substr($dato['B21'], 0, 1) == "=") {
+                        $columna = $columna . "AN,";
+                    }
                     if (substr($dato['RN'], 0, 1) == "=" || $dato['RN'] == "") {
-                        $columna = $columna . "AO,";
+                        $columna = $columna . "AP,";
                     }
                     $dato['caracter'] = "Caracter no permitido, Fila " . $indiceFila . " Columna " . $columna;
                     $this->Model_RequerimientoSurtido->insert_cuenta($dato);
@@ -325,6 +344,8 @@ class RequerimientoSurtidoController extends Controller
             }
         }
         if ($duplicados > 0 || $error > 0) {
+            // print_r("Error:". $error);
+            // print_r("Duplicados:". $duplicados);
             echo "4<a class='btn mb-2 mr-2' style='background-color: #28a745 !important;' href='" . url("RequerimientoSurtido/Excel_Duplicado/{$id_usuario}/{$dato['semana']}") . "'>
                     <svg xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' width='64' height='64' viewBox='0 0 172 172' style=' fill:#000000;'><g fill='none' fill-rule='nonzero' stroke='none' stroke-width='1' stroke-linecap='butt' stroke-linejoin='miter' stroke-miterlimit='10' stroke-dasharray='' stroke-dashoffset='0' font-family='none' font-weight='none' font-size='none' text-anchor='none' style='mix-blend-mode: normal'><path d='M0,172v-172h172v172z' fill='none'></path><g fill='#ffffff'><path d='M94.42993,6.41431c-0.58789,-0.021 -1.17578,0.0105 -1.76367,0.11548l-78.40991,13.83642c-5.14404,0.91333 -8.88135,5.3645 -8.88135,10.58203v104.72852c0,5.22803 3.7373,9.6792 8.88135,10.58203l78.40991,13.83643c0.46191,0.08398 0.93433,0.11548 1.39624,0.11548c1.88965,0 3.71631,-0.65088 5.17554,-1.87915c1.83716,-1.53272 2.88696,-3.7898 2.88696,-6.18335v-12.39819h51.0625c4.44067,0 8.0625,-3.62183 8.0625,-8.0625v-96.75c0,-4.44067 -3.62183,-8.0625 -8.0625,-8.0625h-51.0625v-12.40869c0,-2.38306 -1.0498,-4.64014 -2.88696,-6.17285c-1.36474,-1.15479 -3.05493,-1.80566 -4.8081,-1.87915zM94.34595,11.7998c0.68237,0.06299 1.17578,0.38843 1.43823,0.60889c0.36743,0.30444 0.96582,0.97632 0.96582,2.05762v137.68188c0,1.0918 -0.59839,1.76367 -0.96582,2.06812c-0.35693,0.30444 -1.11279,0.77685 -2.18359,0.58789l-78.40991,-13.83643c-2.57202,-0.45142 -4.44067,-2.677 -4.44067,-5.29102v-104.72852c0,-2.61401 1.86865,-4.8396 4.44067,-5.29102l78.39941,-13.83642c0.27295,-0.04199 0.5249,-0.05249 0.75586,-0.021zM102.125,32.25h51.0625c1.48022,0 2.6875,1.20728 2.6875,2.6875v96.75c0,1.48022 -1.20728,2.6875 -2.6875,2.6875h-51.0625v-16.125h8.0625c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875h-8.0625v-10.75h8.0625c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875h-8.0625v-10.75h8.0625c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875h-8.0625v-10.75h8.0625c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875h-8.0625v-10.75h8.0625c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875h-8.0625zM120.9375,48.375c-1.48022,0 -2.6875,1.19678 -2.6875,2.6875c0,1.49072 1.20728,2.6875 2.6875,2.6875h16.125c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875zM34.46509,53.79199c-0.34643,0.06299 -0.68237,0.18897 -0.99732,0.38843c-1.23877,0.80835 -1.5957,2.47754 -0.78735,3.72681l16.52393,25.40527l-16.52393,25.40527c-0.80835,1.24927 -0.45141,2.91846 0.78735,3.72681c0.46191,0.29395 0.96582,0.43042 1.46973,0.43042c0.87134,0 1.74268,-0.43042 2.25708,-1.21777l15.21167,-23.41064l15.21167,23.41064c0.51441,0.78735 1.38574,1.21777 2.25708,1.21777c0.50391,0 1.00781,-0.13647 1.46973,-0.43042c1.23877,-0.80835 1.5957,-2.47754 0.78735,-3.72681l-16.52393,-25.40527l16.52393,-25.40527c0.80835,-1.24927 0.45142,-2.91846 -0.78735,-3.72681c-1.24927,-0.80835 -2.91846,-0.45141 -3.72681,0.78735l-15.21167,23.41065l-15.21167,-23.41065c-0.60889,-0.93433 -1.70068,-1.36474 -2.72949,-1.17578zM120.9375,64.5c-1.48022,0 -2.6875,1.19678 -2.6875,2.6875c0,1.49072 1.20728,2.6875 2.6875,2.6875h16.125c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875zM120.9375,80.625c-1.48022,0 -2.6875,1.19678 -2.6875,2.6875c0,1.49072 1.20728,2.6875 2.6875,2.6875h16.125c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875zM120.9375,96.75c-1.48022,0 -2.6875,1.19678 -2.6875,2.6875c0,1.49072 1.20728,2.6875 2.6875,2.6875h16.125c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875zM120.9375,112.875c-1.48022,0 -2.6875,1.19678 -2.6875,2.6875c0,1.49072 1.20728,2.6875 2.6875,2.6875h16.125c1.48022,0 2.6875,-1.19678 2.6875,-2.6875c0,-1.49072 -1.20728,-2.6875 -2.6875,-2.6875z'></path></g></g></svg>
                 </a>";
--- app/Models/RequerimientoSurtido.php
+++ app/Models/RequerimientoSurtido.php
@@ -10,22 +10,22 @@ class RequerimientoSurtido extends Model
 {
     use HasFactory;
 
-    
+
     function insert_cuenta($dato, $user_duplicado = null){
         $id_usuario = substr(session('usuario')->usuario_nombres, 0, 1) . session('usuario')->usuario_apater;
         $semana = $dato['semana'];
         if (isset($user_duplicado)) {
-            $sql = "insert into dpedido_lnunot (empresa, costo, pc, pv, pp, pc_b4, pv_b4, pp_b4, 
-                    tipo_usuario, tipo_prenda, codigo_barra, autogenerado, estilo, decripcion, color, 
-                    talla, stock, B01, B02, B03, B04, B05, B06, B07, B08, B09, B10, B11, B13, B14, B15, 
+            $sql = "insert into dpedido_lnunot (empresa, costo, pc, pv, pp, pc_b4, pv_b4, pp_b4,
+                    tipo_usuario, tipo_prenda, codigo_barra, autogenerado, estilo, decripcion, color,
+                    talla, stock, B01, B02, B03, B04, B05, B06, B07, B08, B09, B10, B11, B13, B14, B15,
                     B16, B17, B18, semana, user_reg, estado, fec_reg, user_duplicado, B12, BEC, RN, B19, B20,
                     caracter)
-                    values 
+                    values
                     ('" . $dato['empresa'] . "', '" . $dato['costo'] . "', '" . $dato['pc'] . "', '" . $dato['pv'] . "',
                     '" . $dato['pp'] . "', '" . $dato['pc_b4'] . "', '" . $dato['pv_b4'] . "', '" . $dato['pp_b4'] . "',
-                    '" . $dato['tipo_usuario'] . "', '" . $dato['tipo_prenda'] . "', '" . $dato['codigo_barra'] . "', 
+                    '" . $dato['tipo_usuario'] . "', '" . $dato['tipo_prenda'] . "', '" . $dato['codigo_barra'] . "',
                     '" . $dato['autogenerado'] . "', '" . $dato['estilo'] . "', '" . $dato['decripcion'] . "',
-                    '" . $dato['color'] . "', '" . $dato['talla'] . "', '" . $dato['stock'] . "', 
+                    '" . $dato['color'] . "', '" . $dato['talla'] . "', '" . $dato['stock'] . "',
                     '" . $dato['B01'] . "', '" . $dato['B02'] . "', '" . $dato['B03'] . "',
                     '" . $dato['B04'] . "', '" . $dato['B05'] . "', '" . $dato['B06'] . "', '" . $dato['B07'] . "',
                     '" . $dato['B08'] . "', '" . $dato['B09'] . "', '" . $dato['B10'] . "', '" . $dato['B11'] . "',
@@ -36,26 +36,26 @@ class RequerimientoSurtido extends Model
         } else {
             if ($dato['caracter'] != "") {
 
-                $sql = "insert into dpedido_lnunot (empresa, costo, pc, pv, pp, pc_b4, pv_b4, pp_b4, 
-                        tipo_usuario, tipo_prenda, codigo_barra, autogenerado, estilo, decripcion, color, 
-                        talla, semana, user_reg, estado, fec_reg, BEC, RN, caracter) values 
+                $sql = "insert into dpedido_lnunot (empresa, costo, pc, pv, pp, pc_b4, pv_b4, pp_b4,
+                        tipo_usuario, tipo_prenda, codigo_barra, autogenerado, estilo, decripcion, color,
+                        talla, semana, user_reg, estado, fec_reg, BEC, RN, caracter) values
                         ('" . $dato['empresa'] . "', '" . $dato['costo'] . "', '" . $dato['pc'] . "', '" . $dato['pv'] . "',
                         '" . $dato['pp'] . "', '" . $dato['pc_b4'] . "', '" . $dato['pv_b4'] . "', '" . $dato['pp_b4'] . "',
-                        '" . $dato['tipo_usuario'] . "', '" . $dato['tipo_prenda'] . "', '" . $dato['codigo_barra'] . "', 
+                        '" . $dato['tipo_usuario'] . "', '" . $dato['tipo_prenda'] . "', '" . $dato['codigo_barra'] . "',
                         '" . $dato['autogenerado'] . "', '" . $dato['estilo'] . "', '" . $dato['decripcion'] . "',
-                        '" . $dato['color'] . "', '" . $dato['talla'] . "', 
+                        '" . $dato['color'] . "', '" . $dato['talla'] . "',
                         $semana, '$id_usuario',1, getdate(),
                         '" . $dato['BEC'] . "', '" . $dato['RN'] . "', '" . $dato['caracter'] . "')";
             } else {
-                $sql = "insert into dpedido_lnunot (empresa, costo, pc, pv, pp, pc_b4, pv_b4, pp_b4, 
-                        tipo_usuario, tipo_prenda, codigo_barra, autogenerado, estilo, decripcion, color, 
-                        talla, stock, B01, B02, B03, B04, B05, B06, B07, B08, B09, B10, B11, B13, B14, B15, 
-                        B16, B17, B18, semana, user_reg, estado, fec_reg, B12, BEC, RN, B19, B20) values 
+                $sql = "insert into dpedido_lnunot (empresa, costo, pc, pv, pp, pc_b4, pv_b4, pp_b4,
+                        tipo_usuario, tipo_prenda, codigo_barra, autogenerado, estilo, decripcion, color,
+                        talla, stock, B01, B02, B03, B04, B05, B06, B07, B08, B09, B10, B11, B13, B14, B15,
+                        B16, B17, B18, semana, user_reg, estado, fec_reg, B12, BEC, RN, B19, B20) values
                         ('" . $dato['empresa'] . "', '" . $dato['costo'] . "', '" . $dato['pc'] . "', '" . $dato['pv'] . "',
                         '" . $dato['pp'] . "', '" . $dato['pc_b4'] . "', '" . $dato['pv_b4'] . "', '" . $dato['pp_b4'] . "',
-                        '" . $dato['tipo_usuario'] . "', '" . $dato['tipo_prenda'] . "', '" . $dato['codigo_barra'] . "', 
+                        '" . $dato['tipo_usuario'] . "', '" . $dato['tipo_prenda'] . "', '" . $dato['codigo_barra'] . "',
                         '" . $dato['autogenerado'] . "', '" . $dato['estilo'] . "', '" . $dato['decripcion'] . "',
-                        '" . $dato['color'] . "', '" . $dato['talla'] . "', '" . $dato['stock'] . "', 
+                        '" . $dato['color'] . "', '" . $dato['talla'] . "', '" . $dato['stock'] . "',
                         '" . $dato['B01'] . "', '" . $dato['B02'] . "', '" . $dato['B03'] . "',
                         '" . $dato['B04'] . "', '" . $dato['B05'] . "', '" . $dato['B06'] . "', '" . $dato['B07'] . "',
                         '" . $dato['B08'] . "', '" . $dato['B09'] . "', '" . $dato['B10'] . "', '" . $dato['B11'] . "',
@@ -72,33 +72,81 @@ class RequerimientoSurtido extends Model
         DB::connection('sqlsrv')->update($sql2);
     }
 
-    
+
     function insert_ccuenta($data)
     {
         $id_usuario = substr(session('usuario')->usuario_nombres, 0, 1) . session('usuario')->usuario_apater;
         $semana = $data['semana'];
 
-        $sql3 = "insert into dpedido_lnuno (id_pedido_lnuno, empresa, costo, pc, pv, pp, pc_b4, 
-        pv_b4, pp_b4, tipo_usuario, tipo_prenda, codigo_barra, autogenerado, estilo, decripcion, 
-        color, talla, stock, total, observacion, B01, B02, B03, B04, B05, B06, B07, B08, B09, B10, B11, B13, 
-        B14, B15, B16, B17, B18, B19, B20, semana, anio, estado, user_reg, fec_reg, B12, BEC, RN, archivo) 
+        $sql3 = "insert into dpedido_lnuno (id_pedido_lnuno, empresa, costo, pc, pv, pp, pc_b4,
+        pv_b4, pp_b4, tipo_usuario, tipo_prenda, codigo_barra, autogenerado, estilo, decripcion,
+        color, talla, stock, total, observacion, B01, B02, B03, B04, B05, B06, B07, B08, B09, B10, B11, B13,
+        B14, B15, B16, B17, B18, B19, B20, semana, anio, estado, user_reg, fec_reg, B12, BEC, RN, archivo)
         select " . $data['id_pedido_lnuno'] . ", empresa, costo, pc, pv, pp, pc_b4, pv_b4, pp_b4,
-        '" . $data['tipo_usuario'] . "', tipo_prenda, codigo_barra, autogenerado, estilo, decripcion, 
-        color, talla, stock, total, observacion, B01, B02, B03, B04, B05, B06, B07, B08, B09, B10, 
+        '" . $data['tipo_usuario'] . "', tipo_prenda, codigo_barra, autogenerado, estilo, decripcion,
+        color, talla, stock, total, observacion, B01, B02, B03, B04, B05, B06, B07, B08, B09, B10,
         B11, B13, B14, B15, B16, B17, B18, B19, B20, semana, " . $data['anio'] . ",estado, user_reg, fec_reg, B12, BEC, RN, " . $data['archivo'] . "
-        from dpedido_lnunot where user_reg='$id_usuario' and semana=$semana 
+        from dpedido_lnunot where user_reg='$id_usuario' and semana=$semana
         and tipo_usuario='" . $data['tipo_usuario'] . "'";
         DB::connection('sqlsrv')->insert($sql3);
     }
-    
+
     function get_list_duplicado($usuario, $semana){
-        $sql = "SELECT * FROM dpedido_lnunot where user_reg='" . $usuario . "' and 
+        $sql = "SELECT * FROM dpedido_lnunot where user_reg='" . $usuario . "' and
                 semana=$semana and estado=1 and (caracter is not null or user_duplicado is not null)";
-        
+
         $result = DB::connection('sqlsrv')->select($sql);
 
         // Convertir el resultado a un array
         return json_decode(json_encode($result), true);
     }
 
+    function get_list_duplicadose($dato){
+        $id_usuario = substr(session('usuario')->usuario_nombres, 0, 1) . session('usuario')->usuario_apater;
+        $semana = $dato['semana'];
+        $sql = "SELECT codigo_barra, estilo FROM dpedido_lnunot
+                where user_reg='$id_usuario' and semana=$semana and estado=1
+                GROUP BY codigo_barra, estilo HAVING COUNT(*)>1";
+
+        $result = DB::connection('sqlsrv')->select($sql);
+
+        // Convertir el resultado a un array
+        return json_decode(json_encode($result), true);
+   }
+
+   function ultimo_archivo($dato){
+       $id_usuario = substr(session('usuario')->usuario_nombres, 0, 1) . session('usuario')->usuario_apater;
+       $semana = $dato['semana'];
+
+       $sql = "SELECT isnull(archivo,0) as archivo FROM pedido_lnuno
+               where user_reg='$id_usuario' and semana=$semana
+               GROUP BY archivo";
+
+       $result = DB::connection('sqlsrv')->select($sql);
+       // Convertir el resultado a un array
+       return json_decode(json_encode($result), true);
+   }
+
+   function insert_pedido($dato)
+   {
+       $id_usuario = substr(session('usuario')->usuario_nombres, 0, 1) . session('usuario')->usuario_apater;
+       $semana = $dato['semana'];
+       $sql4 = "insert into pedido_lnuno (tipo_usuario, stock, total, observacion, semana,anio,estado,
+       user_reg, fec_reg, archivo) select tipo_usuario, sum(stock), sum(total), sum(observacion),
+       semana," . $dato['anio'] . ", '1', user_reg, GETDATE(), " . $dato['archivo'] . " from dpedido_lnunot
+       where user_reg='$id_usuario' and semana=$semana group by tipo_usuario, USER_reg, semana";
+       DB::connection('sqlsrv')->insert($sql4);
+   }
+
+   function get_list_pedido($dato)
+   {
+       $id_usuario = substr(session('usuario')->usuario_nombres, 0, 1) . session('usuario')->usuario_apater;
+       $semana = $dato['semana'];
+       $sql = "select * from pedido_lnuno where user_reg='$id_usuario' and semana=$semana
+               and estado=1 and archivo=" . $dato['archivo'] . "";
+
+        $result = DB::connection('sqlsrv')->select($sql);
+        // Convertir el resultado a un array
+        return json_decode(json_encode($result), true);
+   }
 }
