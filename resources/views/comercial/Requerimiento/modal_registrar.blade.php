10bf59f265d2ce5bf76f49352c2fa2b5f8502a3e
dartz24803 <pcardenasfuster@gmail.com>
2024-12-16 09:22:52 -0500

    requerimiento surtido

--- resources/views/comercial/Requerimiento/modal_registrar.blade.php
+++ resources/views/comercial/Requerimiento/modal_registrar.blade.php
@@ -4,13 +4,13 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
         </button>
-    </div>    
-    
+    </div>
+
     <div class="modal-body" style="max-height:450px; overflow:auto;">
         <div class="col-md-12 row mb-2">
             <div class="form-group col-md-2">
                 <label class="col-sm-12 control-label text-bold">Año: </label>
-            </div>  
+            </div>
             <div class="form-group col-md-3">
                 <select class="form-control" id="anio" name="anio">
                     <option value="0">Seleccione</option>
@@ -22,7 +22,7 @@
 
             <div class="form-group col-md-2">
                 <label class="col-sm-12 control-label text-bold">Semana: </label>
-            </div>            
+            </div>
             <div class="form-group col-sm-2">
                 <select class="form-control" id="semana" name="semana">
                     <option value="0">Seleccione</option>
@@ -84,7 +84,7 @@
         <div class="col-md-12 row">
             <div class="form-group col-md-4">
                 <label class="col-sm-12 control-label text-bold">Adjuntar Documento: </label>
-            </div>            
+            </div>
             <div class="form-group col-sm-5">
                 <input type="file" class="form-control-file" id="drequerimiento" name="drequerimiento">
             </div>
@@ -131,6 +131,7 @@
                             mensaje,
                             'success'
                         ).then(function() {
+                            $("#ModalRegistro .close").click()
                             Buscar_Semana();
                         });
                     } else if (validacion == 3) {
